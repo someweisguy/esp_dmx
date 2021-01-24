1921e8d8afea3f28e355c349aa0dcc9d7e7fe085
Mitch Weisbrod <mitch@theweisbrods.com>
2021-01-24 12:37:24 -0800

    add DMX start code macros

--- README.md
+++ README.md
@@ -127,7 +127,6 @@ To determine whether an event is a data corruption or invalid timing event, the 
 ## To Do
 
 - Pass dmx timing checks to user
-- Start code macros
 - Enable C++ compilation/linking.
 - Reset-sequence-first mode: allow for sending of DMX reset sequence first
 - Remote Device Management.
--- include/dmx_caps.h
+++ include/dmx_caps.h
@@ -27,3 +27,99 @@
 #define DMX_TX_MAX_BRK_TO_BRK_US    1000000       // DMX maximum transmittable break-to-break length in microseconds.
 #define DMX_TX_PACKET_TOUT_MS       1000          // DMX host packet timeout in milliseconds.
 #define DMX_TX_PACKET_TOUT_TICK     ((TickType_t)DMX_TX_PACKET_TOUT_MS / portTICK_PERIOD_MS) // DMX host packet timeout in FreeRTOS ticks.
+
+
+/* DMX start codes */
+/**
+ * @brief DMX default NULL start code. A NULL start code identifies subsequent
+ * data slots as a block of untyped sequential 8-bit information. Packets
+ * identified by a NULL start code are the default packets sent on DMX 
+ * networks.
+ */
+#define DMX_SC     0x00
+
+/**
+ * @brief Remote Device Management (RDM) start code. RDM is an extension to
+ * DMX. A key goal of the RDM standard is to allow the use of new and legacy
+ * DMX receiving devices in mixed systems with new RDM equipment and to provide
+ * a straightforward path to upgrade existing DMX distribution systems for
+ * support of the RDM protocol. The use of RDM devices in a DMX system will not
+ * compromise any DMX functionality. 
+ */
+#define RDM_SC     0xcc
+
+/**
+ * @brief ASCII Text alternate start code. Alternate start code 0x17 designates
+ * a special packet of between 3 and 512 data slots. The purpose of the ASCII
+ * text packet is to allow equipment to send diagnostic information coded per
+ * the American Standard Code for Information Interchange and formatted for 
+ * display.
+ * 
+ * Slot allocation is as follows:
+ *   Slot 1: Page number of one of the possible 256 text pages.
+ *   Slot 2: Characters per line. Indicates the number of characters per line
+ *     that the transmitting device has used for the purposes of formatting the
+ *     text. A slot value of zero indicates ignore this field.
+ *   Slots 3-512: Consecutive display characters in ASCII format. All
+ *     characters are allowed and where a DMX512 text viewer is capable, it
+ *     shall display the data using the ISO/IEC 646 standard character set. A
+ *     slot value of zero shall terminate the ASCII string. Slots transmitted 
+ *     after this null terminator up to the reset sequence shall be ignored.
+ */
+#define DMX_TEXT_ASC     0x17 
+
+/**
+ * @brief Test Packet alternate start code. Alternate start code 0x55 designates a
+ * special test packet of 512 data slots, where all data slots carry the value
+ * 0x55. Test packets shall be sent so that the time from the start of the
+ * break until the stop bit of the 513th slot shall be no more than 25
+ * milliseconds. When test packets are sent back to back, the mark-before-break
+ * time shall be no more than 88 microseconds. The break timing for test
+ * packets shall be greater than or equal to 88 microseconds, and less than or
+ * equal to 120 microseconds. The mark-after-break time shall be greater than
+ * or equal to 8 microseconds and less than or equal to 16 microseconds.
+ */
+#define DMX_TEST_ASC     0x55
+
+/**
+ * @brief UTF-8 Text Packet alternate start code. Alternate start code 0x90
+ * designates a special packet of between 3 and 512 data slots. The purpose of
+ * the UTF-8 Text Packet is to allow equipment to send diagnostic information
+ * coded per UTF-8 as described in Unicode 5.0 published by The Unicode
+ * Consortium and formatted for display. UTF-8 should only be used when the
+ * text packet cannot be expressed in ASCII using the DMX_TEXT_ASC start code.
+ * 
+ * Slot allocation is as follows:
+ *   Slot 1: Page number of one of the possible 256 text pages.
+ *   Slot 2: Characters per Line. Indicates the number of characters per line
+ *     that the transmitting device has used for the purposes of formatting the
+ *     text. A slot value of zero indicates "Ignore this field."
+ *   Slots 3-512: Consecutive display characters in UTF-8 format. All
+ *     characters are allowed and where a DMX512 text viewer is capable, it
+ *     shall display the data using the Unicode 5.0 character set. A slot value
+ *     of zero shall terminate the UTF-8 text string. Slots transmitted after
+ *     this null terminator up to the reset sequence shall be ignored.
+ */
+#define DMX_UTF8_ASC     0x90
+
+/**
+ * @brief Manufacturer/Organization ID alternate start code. Alternate start
+ * code 0x91 followed by a 2 byte manufacturer ID field is reserved for 
+ * Manufacturer/Organization specific use, transmitted byte order is MSB, LSB.
+ * The next byte after the manufacturers ID would normally be a manufacturer’s
+ * sub-code
+ */
+#define DMX_ORG_ID_ASC   0x91
+
+/**
+ * @brief System Information Packet alternate start code. Alternate startcode
+ * 0xCF is reserved for a System Information Packet (SIP). The SIP includes a
+ * method of sending checksum data relating to the previous NULL start code
+ * packet on the data link and other control information. No other packet shall
+ * be sent between the NULL start code packet and the SIP that carries its 
+ * checksum.
+ * 
+ * For more information on the System Information Packet alternate start code,
+ * see annex D5 in the ANSI-ESTA E1.11 DMX512-A standards document.
+ */
+#define DMX_SIP_ASC      0xcf
