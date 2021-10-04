3a73ce76afdbe30a7ba81be4751df51ed3b7f7f8
Mitch Weisbrod <mitch@theweisbrods.com>
2021-10-03 19:49:14 -0700

    put includes in brackets

--- examples/DMXTiming/DMXTiming.ino
+++ examples/DMXTiming/DMXTiming.ino
@@ -12,8 +12,8 @@
   https://github.com/someweisguy/esp_dmx
 
 */
-#include "Arduino.h"
-#include "esp_dmx.h"
+#include <Arduino.h>
+#include <esp_dmx.h">
 
 /* First, lets define the hardware pins that we are using with our ESP32. We
   need to define which pin is transmitting data and which pin is receiving data.
