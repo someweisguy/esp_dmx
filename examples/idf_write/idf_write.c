a930acc1bd77f47d81c106e0e70b29ecaffdcaf4
Mitch Weisbrod <mitch@theweisbrods.com>
2022-06-20 20:15:11 -0700

    update example

--- examples/idf_write/idf_write.c
+++ examples/idf_write/idf_write.c
@@ -3,11 +3,12 @@
   ESP-IDF DMX Write
 
   Writes data to the DMX bus. The value of every byte in the DMX packet (except
-  the start code) is incremented by 1 every 1 second.
+  the start code) is incremented by 1 every 1 second. After the twentieth packet
+  update, the DMX driver will be uninstalled and the program will terminate.
 
   Note: this example is for use with the ESP-IDF. It will not work on Arduino!
 
-  Created 28 January 2021
+  Created 20 June 2022
   By Mitch Weisbrod
 
   https://github.com/someweisguy/esp_dmx
@@ -23,51 +24,54 @@
 
 static const char* TAG = "main";
 
-// declare the user buffer to write DMX data
+// declare the user buffer to read in DMX data
 static uint8_t data[DMX_MAX_PACKET_SIZE] = {};
 
 void app_main() {
   // use DMX port 2
   const dmx_port_t dmx_num = DMX_NUM_2;
 
-  // configure the UART hardware to the default DMX settings
-  const dmx_config_t dmx_config = DMX_DEFAULT_CONFIG;
-  ESP_ERROR_CHECK(dmx_param_config(dmx_num, &dmx_config));
-
   // set communications pins
   ESP_ERROR_CHECK(dmx_set_pin(dmx_num, TX_PIN, RX_PIN, EN_PIN));
 
   // initialize the DMX driver without an event queue
-  ESP_ERROR_CHECK(dmx_driver_install(dmx_num, DMX_MAX_PACKET_SIZE, 0, NULL, 1));
-
-  // set DMX to TX mode
+  dmx_config_t driver_config = DMX_DEFAULT_CONFIG;
+  ESP_ERROR_CHECK(dmx_driver_install(dmx_num, &driver_config, 0, NULL));
   dmx_set_mode(dmx_num, DMX_MODE_WRITE);
 
-  // keeps track of how often we are logging messages to console
-  uint32_t packet_counter = 0;
-  uint8_t inc_value = 0;
+  // keep track of when we update the packet
+  TickType_t next_tick = xTaskGetTickCount() + (1000 / portTICK_PERIOD_MS);
+  bool update = true;
+  int num_updates = 0;
 
   while (1) {
-    // block until the packet is done being sent
-    dmx_wait_send_done(dmx_num, DMX_TX_PACKET_TOUT_TICK);
+    const TickType_t now_tick = xTaskGetTickCount();
 
-    // transmit the packet on the DMX bus
     dmx_send_packet(dmx_num, DMX_MAX_PACKET_SIZE);
 
-    // increment the packet counter
-    ++packet_counter;
-
-    // increment every data slot in the frame by 1 every 1 second (44fps)
-    if (packet_counter >= 44) {
-      ESP_LOGI(TAG, "incrementing data to %02X", ++inc_value);
-      // don't increment the start code
-      for (int i = 1; i < DMX_MAX_PACKET_SIZE; ++i) data[i]++;
-
-      // write the packet to the DMX driver
-      dmx_write_packet(dmx_num, data, DMX_MAX_PACKET_SIZE);
-
-      // decrement our packet counter timer
-      packet_counter -= 44;
+    if (now_tick >= next_tick) {
+      for (int i = 1; i < DMX_MAX_PACKET_SIZE; ++i) ++data[i];
+      next_tick = now_tick + (1000 / portTICK_PERIOD_MS);
+      update = true;
+      ++num_updates;
     }
+
+    if (update) {
+      dmx_wait_write_sync(dmx_num, DMX_TX_PACKET_TOUT_TICK);
+      dmx_write_packet(dmx_num, data, DMX_MAX_PACKET_SIZE);
+      ESP_LOGI(TAG, "incrementing to %02X", data[1]);
+      update = false;
+    }
+
+    dmx_wait_sent(dmx_num, DMX_TX_PACKET_TOUT_TICK);
+
+    if (num_updates > 30) {
+      ESP_LOGI(TAG, "uninstalling DMX driver");
+      dmx_driver_delete(dmx_num);
+      break;
+    }
+
+    // 40ms == ~25fps bytestream
+    vTaskDelayUntil(&now_tick, 40 / portTICK_PERIOD_MS);
   }
 }
