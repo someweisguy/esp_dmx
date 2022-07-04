bb019e26acda2d19478a06fec35a60e5ea600d99
Mitch Weisbrod <mitch@theweisbrods.com>
2022-07-03 19:18:35 -0700

    fix type warning

--- src/impl/intr_handlers.h
+++ src/impl/intr_handlers.h
@@ -348,7 +348,7 @@ static void IRAM_ATTR dmx_timing_intr_handler(void *arg) {
 static bool IRAM_ATTR dmx_timer_intr_handler(void *arg) {
   dmx_driver_t *const driver = (dmx_driver_t *)arg;
   dmx_context_t *const hardware = &dmx_context[driver->dmx_num];
-  bool task_awoken = false;
+  int task_awoken = false;
 
   if (driver->awaiting_response) {
     // Handle DMX timeouts
