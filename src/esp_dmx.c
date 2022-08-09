d72dcb3dd007bfa6299d53489ab2e3781e66ea77
Mitch Weisbrod <mitch@theweisbrods.com>
2022-08-08 20:54:59 -0700

    todos

--- src/esp_dmx.c
+++ src/esp_dmx.c
@@ -312,6 +312,7 @@ esp_err_t dmx_get_mab_len(dmx_port_t dmx_num, uint32_t *mab_len) {
 /// Read/Write  ###############################################################
 size_t dmx_read(dmx_port_t dmx_num, void *destination, size_t size) {
   // TODO: Check arguments
+  // TODO: clamp size
 
   dmx_driver_t *const driver = dmx_driver[dmx_num];
 
@@ -323,6 +324,7 @@ size_t dmx_read(dmx_port_t dmx_num, void *destination, size_t size) {
 
 size_t dmx_write(dmx_port_t dmx_num, const void *source, size_t size) {
   // TODO: check args
+  // TODO: clamp size
 
   dmx_driver_t *const driver = dmx_driver[dmx_num];
   dmx_context_t *const hardware = &dmx_context[dmx_num];
