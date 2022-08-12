b97a9a97ae9ee182b53bc9f79c18a092e267b577
Mitch Weisbrod <mitch@theweisbrods.com>
2022-08-11 20:56:58 -0700

    use macro for manufactuer id

--- src/esp_rdm.c
+++ src/esp_rdm.c
@@ -5,6 +5,8 @@
 #include "endian.h"
 #include "esp_system.h"
 
+#define DMX_DEFAULT_MANUFACTURER_ID (0xbeef)  // TODO: Use real manufactuer ID
+
 static rdm_uid_t rdm_uid = {};  // The 48-bit unique ID of this device.
 
 rdm_uid_t rdm_get_uid() { 
@@ -12,7 +14,7 @@ rdm_uid_t rdm_get_uid() {
   if (rdm_uid.raw == 0) {
     uint8_t mac[8];
     esp_efuse_mac_get_default(mac);
-    rdm_uid.manufacturer_id = 0xbeef;  // FIXME: use real manufacturer ID
+    rdm_uid.manufacturer_id = DMX_DEFAULT_MANUFACTURER_ID;
     rdm_uid.device_id = bswap32(*(uint32_t *)(mac + 2));  // Don't use MAC OUI
   }
 
