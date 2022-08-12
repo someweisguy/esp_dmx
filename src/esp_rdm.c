7601bb8cbc701d38d606142424758c488b5f560f
Mitch Weisbrod <mitch@theweisbrods.com>
2022-08-11 20:30:39 -0700

    don't use MAC OUI in UID

--- src/esp_rdm.c
+++ src/esp_rdm.c
@@ -2,6 +2,7 @@
 
 #include <string.h>
 
+#include "endian.h"
 #include "esp_system.h"
 
 static rdm_uid_t rdm_uid = {};  // The 48-bit unique ID of this device.
@@ -12,7 +13,7 @@ rdm_uid_t rdm_get_uid() {
     uint8_t mac[8];
     esp_efuse_mac_get_default(mac);
     rdm_uid.manufacturer_id = 0xbeef;  // FIXME: use real manufacturer ID
-    memcpy(&rdm_uid.device_id, mac, 4);
+    rdm_uid.device_id = bswap32(*(uint32_t *)(mac + 2));  // Don't use MAC OUI
   }
 
   return rdm_uid;
