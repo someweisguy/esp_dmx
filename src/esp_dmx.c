6f67d4a7f9e99793dc0d7595b311962f4353b66e
Mitch Weisbrod <mitch@theweisbrods.com>
2022-08-20 23:12:22 -0700

    add enum name

--- src/esp_dmx.c
+++ src/esp_dmx.c
@@ -25,7 +25,7 @@
 
 static const char *TAG = "dmx";  // The log tagline for the file.
 
-enum {
+enum rdm_packet_spacing {
   RDM_DISCOVERY_NO_RESPONSE_PACKET_SPACING = 5800,  // The amount of time that must pass between the RDM controller sending a discovery packet and then sending any other packet.
   RDM_REQUEST_NO_RESPONSE_PACKET_SPACING = 3000,    // The amount of time that must pass between the RDM controller sending a request, considering the response lost, and sending any other packet.
   RDM_BROADCAST_PACKET_SPACING = 176,               // The amount of time that must pass between the RDM controller sending a broadcast packet and then sending any other packet.
