bbbcaf068f116f0bd04c09a89960f13f22439714
Mitch Weisbrod <mitch@theweisbrods.com>
2022-08-28 11:30:19 -0700

    convert defines to enums

--- src/esp_dmx.c
+++ src/esp_dmx.c
@@ -15,9 +15,6 @@
 #include "impl/intr_handlers.h"
 #include "rdm_tools.h"
 
-#define DMX_UART_FULL_DEFAULT  (1)  // The default value for the RX FIFO full interrupt threshold.
-#define DMX_UART_EMPTY_DEFAULT (8)  // The default value for the TX FIFO empty interrupt threshold.
-
 // Initializes the DMX context.
 #define DMX_CONTEXT_INIT(uart_num)                                 \
   {                                                                \
@@ -29,11 +26,16 @@ static const char *TAG = "dmx";  // The log tagline for the file.
 
 enum rdm_packet_spacing {
   RDM_DISCOVERY_NO_RESPONSE_PACKET_SPACING = 5800,  // The amount of time that must pass between the RDM controller sending a discovery packet and then sending any other packet.
-  RDM_REQUEST_NO_RESPONSE_PACKET_SPACING = 3000,    // The amount of time that must pass between the RDM controller sending a request, considering the response lost, and sending any other packet.
-  RDM_BROADCAST_PACKET_SPACING = 176,               // The amount of time that must pass between the RDM controller sending a broadcast packet and then sending any other packet.
-  RDM_RESPOND_TO_REQUEST_PACKET_SPACING = 176,      // The amount of time that must pass between the RDM controller sending a request and the RDM responder sending a response packet.
+  RDM_REQUEST_NO_RESPONSE_PACKET_SPACING = 3000,  // The amount of time that must pass between the RDM controller sending a request, considering the response lost, and sending any other packet.
+  RDM_BROADCAST_PACKET_SPACING = 176,  // The amount of time that must pass between the RDM controller sending a broadcast packet and then sending any other packet.
+  RDM_RESPOND_TO_REQUEST_PACKET_SPACING = 176,  // The amount of time that must pass between the RDM controller sending a request and the RDM responder sending a response packet.
 
-  RDM_RESPONSE_LOST_TIMEOUT = 2800,         // The amount of time that must pass before the RDM controller considers a discovery response packet lost. 
+  RDM_RESPONSE_LOST_TIMEOUT = 2800,  // The amount of time that must pass before the RDM controller considers a discovery response packet lost. 
+};
+
+enum dmx_default_interrupt_values {
+  DMX_UART_FULL_DEFAULT = 1,  // The default value for the RX FIFO full interrupt threshold.
+  DMX_UART_EMPTY_DEFAULT = 8,  // The default value for the TX FIFO empty interrupt threshold.
 };
 
 DRAM_ATTR dmx_context_t dmx_context[DMX_NUM_MAX] = {
