e4b071e229dc56e26509dd82553a64f9aa02fc68
Mitch Weisbrod <mitch@theweisbrods.com>
2022-09-05 10:32:25 -0700

    docs

--- src/rdm_tools.h
+++ src/rdm_tools.h
@@ -81,10 +81,12 @@ bool rdm_parse(void *data, size_t size, rdm_event_t *event);
 // TODO: docs
 size_t rdm_send_disc_response(dmx_port_t dmx_num);
 
+// TODO: docs
 size_t rdm_send_disc_un_mute(dmx_port_t dmx_num, uint64_t uid,
                              dmx_event_t *event, size_t *num_params,
                              rdm_disc_mute_param_t *params);
 
+// TODO: docs
 size_t rdm_send_disc_mute(dmx_port_t dmx_num, uint64_t uid, dmx_event_t *event,
                           size_t *num_params, rdm_disc_mute_param_t *params);
 
