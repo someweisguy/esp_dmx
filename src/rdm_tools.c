b9596c9bad33370eb1968da6bfead2bcf84c099c
Mitch Weisbrod <mitch@theweisbrods.com>
2022-09-04 22:18:38 -0700

    add TODOs

--- src/rdm_tools.c
+++ src/rdm_tools.c
@@ -221,7 +221,8 @@ bool rdm_send_discovery_unmute(dmx_port_t dmx_num, uint64_t uid,
 bool rdm_send_discovery_mute(dmx_port_t dmx_num, uint64_t uid,
                              rdm_event_t *event, int *control_field,
                              uint64_t *binding_uid) {
-  // FIXME: bool rdm_func(dmx_num, uid, *rdm_event_t, *num_params, *params);
+  // FIXME: bool rdm_func(dmx_num, uid, *dmx_event_t, *num_params, *params);
+  // FIXME: use dmx_event_t instead of rdm_event_t
   // TODO: check args
   
   dmx_driver_t *const driver = dmx_driver[dmx_num];
