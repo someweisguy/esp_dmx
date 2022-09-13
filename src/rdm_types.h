1c1002cccf45c62963efd6a80d4455a4f9a7bb36
Mitch Weisbrod <mitch@theweisbrods.com>
2022-09-12 20:22:03 -0700

    remove mute params type

--- src/rdm_types.h
+++ src/rdm_types.h
@@ -16,17 +16,6 @@
 extern "C" {
 #endif
 
-/**
- * @brief The parameter data area included in the responses to mute and un-mute
- * messages includes a control field to inform the controller about specific
- * properties of the device, and may include an optional binding UID field to
- * indicate the physical arrangement of the responding device.
- */
-typedef struct rdm_disc_mute_param {
-  uint16_t control_field;  // Informs the RDM controller about specific properties of the device. The managed proxy flag (bit 0) shall be set when the responder is a proxy device. The sub-device flag (bit 1) shall be set when the responder supports sub-devices. The boot-loader flag (bit 2) shall only be set when the device is incapable of normal operation until receiving a firmware upload. The proxied device flag (bit 3) shall only be set when a proxy is responding to discovery on behalf of another device.
-  uint64_t binding_uid;    // Shall only be included when the responding device contains multiple responder ports. If the device does contain multiple ports, then the Binding UID field shall contain the UID for the primary port on the device. Is 0 if no binding UID is received.
-} rdm_disc_mute_param_t;
-
 #ifdef __cplusplus
 }
 #endif
