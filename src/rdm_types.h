a2ff2914bf12a0ba1768702e47ee432203f2a7b1
Mitch Weisbrod <mitch@theweisbrods.com>
2022-09-25 16:03:12 -0700

    update file name

--- src/rdm_types.h
+++ src/rdm_types.h
@@ -2,7 +2,7 @@
  * @file dmx_types.h
  * @author Mitch Weisbrod
  * @brief This file contains the types used for processing RDM as needed in
- * rdm_tools.h. Types that are used by the base driver should be defined in 
+ * esp_rdm.h. Types that are used by the base driver should be defined in 
  * dmx_types.h instead. Anonymous enums and constants defined in the DMX or RDM
  * standard should be defined in dmx_constants.h or rdm_constants.h instead.
  */
