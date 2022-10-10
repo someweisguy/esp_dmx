b3423b3412edd1338bd04d52f4a05618d0b81172
Mitch Weisbrod <mitch@theweisbrods.com>
2022-10-09 17:56:23 -0700

    remove unused funcs

--- src/esp_rdm.h
+++ src/esp_rdm.h
@@ -50,25 +50,6 @@ void rdm_set_uid(rdm_uid_t uid);
  */
 bool rdm_is_muted();
 
-/**
- * @brief Helper function to parse RDM data into a user-friendly format.
- *
- * @param source A pointer to the RDM data buffer.
- * @param size The size of the RDM data buffer.
- * @param[out] header A structure containing information about the RDM packet.
- * @return true if the data is a valid RDM packet.
- * @return false if the data is not a valid RDM packet.
- */
-bool rdm_decode_header(const void *source, size_t size, rdm_header_t *header);
-
-// TODO: docs, returns number of params able to be decoded
-size_t rdm_decode_params(const void *source, size_t size, void *params,
-                         size_t num_params, size_t message_num);
-
-// TODO: docs, returns number of bytes written to destination
-size_t rdm_encode(void *destination, size_t size, const rdm_header_t *header,
-                  const void *params, size_t num_params, size_t message_num);
-
 // TODO: docs
 size_t rdm_send_disc_response(dmx_port_t dmx_num, rdm_uid_t uid);
 
