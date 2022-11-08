06fb6839f195e898f3ada53cb48b6e2f88fc2ec9
Mitch Weisbrod <mitch@theweisbrods.com>
2022-11-07 21:43:52 -0800

    remove unused type

--- src/private/rdm_encode/types.h
+++ src/private/rdm_encode/types.h
@@ -33,12 +33,6 @@ typedef struct __attribute__((__packed__)) rdm_data {
   } pd;                        // The parameter data (PD) is of variable length. The content format is PID dependent.
 } rdm_data_t;
 
-// TODO: can we replace this with a uid[2]?
-typedef struct __attribute__((__packed__)) rdm_disc_unique_branch_data {
-  uint8_t lower_bound[6];
-  uint8_t upper_bound[6];
-} rdm_disc_unique_branch_data_t;
-
 // TODO: docs
 typedef struct __attribute__((__packed__)) rdm_disc_mute_data {
   union {
