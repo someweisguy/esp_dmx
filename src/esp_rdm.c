d0b95627621e85e1afd361a8fc62fb6592a54878
Mitch Weisbrod <mitch@theweisbrods.com>
2022-11-08 21:25:34 -0800

    fix return types

--- src/esp_rdm.c
+++ src/esp_rdm.c
@@ -59,8 +59,8 @@ void rdm_set_uid(dmx_port_t dmx_num, rdm_uid_t uid) {
 }
 
 bool rdm_is_muted(dmx_port_t dmx_num) { 
-  RDM_CHECK(dmx_num < DMX_NUM_MAX, , "dmx_num error");
-  RDM_CHECK(dmx_driver_is_installed(dmx_num), , "driver is not installed");
+  RDM_CHECK(dmx_num < DMX_NUM_MAX, false, "dmx_num error");
+  RDM_CHECK(dmx_driver_is_installed(dmx_num), false, "driver is not installed");
   
   spinlock_t *const restrict spinlock = &dmx_spinlock[dmx_num];
   dmx_driver_t *const driver = dmx_driver[dmx_num];
