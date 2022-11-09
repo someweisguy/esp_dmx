388ca10dd703bbdfc995a1d100092e9ca8ae68ee
Mitch Weisbrod <mitch@theweisbrods.com>
2022-11-08 20:16:17 -0800

    documentation

--- src/private/dmx_hal.h
+++ src/private/dmx_hal.h
@@ -31,7 +31,7 @@ extern "C" {
 /**
  * @brief Initializes the UART for DMX.
  *
- * @param hal A pointer to a UART HAL context.
+ * @param uart A pointer to a UART port.
  */
 void dmx_uart_init(uart_dev_t *uart) {
   // Configure the UART for DMX output
@@ -56,7 +56,7 @@ void dmx_uart_init(uart_dev_t *uart) {
 /**
  * @brief Gets the UART baud rate of the selected UART hardware.
  *
- * @param hal A pointer to a UART HAL context.
+ * @param uart A pointer to a UART port.
  * @return The baud rate of the UART hardware.
  */
 uint32_t dmx_uart_get_baud_rate(uart_dev_t *uart) {
@@ -66,7 +66,7 @@ uint32_t dmx_uart_get_baud_rate(uart_dev_t *uart) {
 /**
  * @brief Sets the baud rate for the UART.
  *
- * @param hal A pointer to a UART HAL context.
+ * @param uart A pointer to a UART port.
  * @param baud_rate The baud rate to use.
  */
 void dmx_uart_set_baud_rate(uart_dev_t *uart, uint32_t baud_rate) {
@@ -77,7 +77,7 @@ void dmx_uart_set_baud_rate(uart_dev_t *uart, uint32_t baud_rate) {
  * @brief Sets the number of bytes that the UART must receive to trigger a RX
  * FIFO full interrupt.
  *
- * @param hal A pointer to a UART HAL context.
+ * @param uart A pointer to a UART port.
  * @param threshold The number of bytes needed to trigger an RX FIFO full
  * interrupt.
  */
@@ -89,7 +89,7 @@ void dmx_uart_set_rxfifo_full(uart_dev_t *uart, uint8_t threshold) {
  * @brief Sets the number of bytes that the UART TX FIFO must have remaining in
  * it to trigger a TX FIFO empty interrupt.
  *
- * @param hal A pointer to a UART HAL context.
+ * @param uart A pointer to a UART port.
  * @param threshold The number of bytes remaining to trigger a TX FIFO empty
  * interrupt.
  */
@@ -100,7 +100,7 @@ void dmx_uart_set_txfifo_empty(uart_dev_t *uart, uint8_t threshold) {
 /**
  * @brief Inverts or un-inverts the TX line on the UART.
  *
- * @param hal A pointer to a UART HAL context.
+ * @param uart A pointer to a UART port.
  * @param invert_mask 1 to invert, 0 to un-invert.
  */
 DMX_ISR_ATTR void dmx_uart_invert_tx(uart_dev_t *uart, uint32_t invert) {
@@ -124,7 +124,7 @@ DMX_ISR_ATTR void dmx_uart_invert_tx(uart_dev_t *uart, uint32_t invert) {
 /**
  * @brief Gets the level of the UART RTS line.
  *
- * @param hal A pointer to a UART HAL context.
+ * @param uart A pointer to a UART port.
  * @return 1 if the UART RTS line is enabled (set low; read), 0 if the UART RTS
  * line is disable (set high; write).
  */
@@ -149,7 +149,7 @@ int dmx_uart_get_rts(uart_dev_t *uart) {
 /**
  * @brief Gets the interrupt status mask from the UART.
  *
- * @param hal A pointer to a UART HAL context.
+ * @param uart A pointer to a UART port.
  * @return The interrupt status mask.
  */
 DMX_ISR_ATTR int dmx_uart_get_interrupt_status(uart_dev_t *uart) {
@@ -159,7 +159,7 @@ DMX_ISR_ATTR int dmx_uart_get_interrupt_status(uart_dev_t *uart) {
 /**
  * @brief Enables UART interrupts using an interrupt mask.
  *
- * @param hal A pointer to a UART HAL context.
+ * @param uart A pointer to a UART port.
  * @param mask The UART mask that is enabled.
  */
 DMX_ISR_ATTR void dmx_uart_enable_interrupt(uart_dev_t *uart, int mask) {
@@ -169,7 +169,7 @@ DMX_ISR_ATTR void dmx_uart_enable_interrupt(uart_dev_t *uart, int mask) {
 /**
  * @brief Disables UART interrupts using an interrupt mask.
  *
- * @param hal A pointer to a UART HAL context.
+ * @param uart A pointer to a UART port.
  * @param mask The UART mask that is disabled.
  */
 DMX_ISR_ATTR void dmx_uart_disable_interrupt(uart_dev_t *uart, int mask) {
@@ -179,7 +179,7 @@ DMX_ISR_ATTR void dmx_uart_disable_interrupt(uart_dev_t *uart, int mask) {
 /**
  * @brief Clears UART interrupts using a mask.
  *
- * @param hal A pointer to a UART HAL context.
+ * @param uart A pointer to a UART port.
  * @param mask The UART mask that is cleared.
  */
 DMX_ISR_ATTR void dmx_uart_clear_interrupt(uart_dev_t *uart, int mask) {
@@ -189,7 +189,7 @@ DMX_ISR_ATTR void dmx_uart_clear_interrupt(uart_dev_t *uart, int mask) {
 /**
  * @brief Gets the current length of the bytes in the UART RX FIFO.
  *
- * @param hal A pointer to a UART HAL context.
+ * @param uart A pointer to a UART port.
  * @return The number of bytes in the UART RX FIFO.
  */
 DMX_ISR_ATTR uint32_t dmx_uart_get_rxfifo_len(uart_dev_t *uart) {
@@ -199,7 +199,7 @@ DMX_ISR_ATTR uint32_t dmx_uart_get_rxfifo_len(uart_dev_t *uart) {
 /**
  * @brief Gets the level of the UART RX line.
  *
- * @param hal A pointer to a UART HAL context.
+ * @param uart A pointer to a UART port.
  * @return The UART RX line level.
  */
 
@@ -224,7 +224,7 @@ DMX_ISR_ATTR uint32_t dmx_uart_get_rx_level(uart_dev_t *uart) {
 /**
  * @brief Reads from the UART RX FIFO.
  *
- * @param hal A pointer to a UART HAL context.
+ * @param uart A pointer to a UART port.
  * @param buf Destination buffer to be read into.
  * @param num The maximum number of characters to read. Set to 0 to read all
  * data.
@@ -234,7 +234,7 @@ DMX_ISR_ATTR void dmx_uart_read_rxfifo(uart_dev_t *uart, uint8_t *buf,
                                        int *size) {
   const size_t rxfifo_len = uart_ll_get_rxfifo_len(uart);
   if (*size > rxfifo_len) {
-      *size = rxfifo_len;
+    *size = rxfifo_len;
   }
   uart_ll_read_rxfifo(uart, buf, *size);
 }
@@ -242,7 +242,7 @@ DMX_ISR_ATTR void dmx_uart_read_rxfifo(uart_dev_t *uart, uint8_t *buf,
 /**
  * @brief Enables or disables the UART RTS line.
  *
- * @param hal A pointer to a UART HAL context.
+ * @param uart A pointer to a UART port.
  * @param set 1 to enable the UART RTS line (set low; read), 0 to disable the
  * UART RTS line (set high; write).
  */
@@ -253,7 +253,7 @@ DMX_ISR_ATTR void dmx_uart_set_rts(uart_dev_t *uart, int set) {
 /**
  * @brief Resets the UART RX FIFO.
  *
- * @param hal A pointer to a UART HAL context.
+ * @param uart A pointer to a UART port.
  */
 DMX_ISR_ATTR void dmx_uart_rxfifo_reset(uart_dev_t *uart) {
   uart_ll_rxfifo_rst(uart);
@@ -262,7 +262,7 @@ DMX_ISR_ATTR void dmx_uart_rxfifo_reset(uart_dev_t *uart) {
 /**
  * @brief Gets the length of the UART TX FIFO.
  *
- * @param hal A pointer to a UART HAL context.
+ * @param uart A pointer to a UART port.
  * @return The length of the UART TX FIFO.
  */
 DMX_ISR_ATTR uint32_t dmx_uart_get_txfifo_len(uart_dev_t *uart) {
@@ -272,7 +272,7 @@ DMX_ISR_ATTR uint32_t dmx_uart_get_txfifo_len(uart_dev_t *uart) {
 /**
  * @brief Writes to the UART TX FIFO.
  *
- * @param hal A pointer to a UART HAL context.
+ * @param uart A pointer to a UART port.
  * @param buf The source buffer from which to write.
  * @param size The number of bytes to write.
  */
@@ -286,7 +286,7 @@ DMX_ISR_ATTR void dmx_uart_write_txfifo(uart_dev_t *uart, const void *buf,
 /**
  * @brief Resets the UART TX FIFO.
  *
- * @param hal A pointer to a UART HAL context.
+ * @param uart A pointer to a UART port.
  */
 DMX_ISR_ATTR void dmx_uart_txfifo_reset(uart_dev_t *uart) {
   uart_ll_txfifo_rst(uart);
