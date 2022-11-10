cc651718254279d0fbe0f828be6d1b2583b97be4
Mitch Weisbrod <mitch@theweisbrods.com>
2022-11-09 19:30:07 -0800

    refactors

--- src/esp_rdm.c
+++ src/esp_rdm.c
@@ -775,43 +775,51 @@ size_t rdm_get_dmx_start_address(dmx_port_t dmx_num, rdm_uid_t uid,
   size_t num_params = 0;
   dmx_event_t event;
   const size_t read = dmx_receive(dmx_num, &event, pdMS_TO_TICKS(20));
-  if (event.err) {
-    response->err = event.err;
-    response->num_params = 0;
-  } else if (read) {
+  if (!read) {
+    if (response != NULL) {
+      response->err = event.err;
+      response->type = RDM_RESPONSE_TYPE_NONE;
+      response->num_params = 0;
+    }
+  } else  {
     // Parse the response to ensure it is valid
+    esp_err_t err;
     if (!rdm_decode_header(driver->data.buffer, &header)) {
-      response->err = ESP_ERR_INVALID_RESPONSE;
+      err = ESP_ERR_INVALID_RESPONSE;
     } else if (!header.checksum_is_valid) {
-      response->err = ESP_ERR_INVALID_CRC;
+      err = ESP_ERR_INVALID_CRC;
     } else if (header.destination_uid != rdm_get_uid(dmx_num)) {
-      response->err = ESP_ERR_INVALID_ARG;
+      err = ESP_ERR_INVALID_ARG;
     } else {
-      response->err = ESP_OK;
+      err = ESP_OK;
     }
 
     // Handle the parameter data
-    response->type = header.response_type;
+    uint32_t response_val;
     if (header.response_type == RDM_RESPONSE_TYPE_ACK) {
-      // Decode the parameter data
-      uint32_t param;
-      num_params = rdm_decode_16bit(&rdm->pd, &param, 1);
-      *start_address = param;
+      // Decode the parameter data     
+      response_val = rdm_decode_16bit(&rdm->pd, (uint32_t *)&start_address, 1);
       response->num_params = num_params;
     } else if (header.response_type == RDM_RESPONSE_TYPE_ACK_TIMER) {
       // Get the estimated response time and convert it to FreeRTOS ticks
-      uint32_t estimated_response_time;
-      rdm_decode_16bit(&rdm->pd, &estimated_response_time, 1);
-      response->timer = pdMS_TO_TICKS(estimated_response_time * 10);
+      rdm_decode_16bit(&rdm->pd, &response_val, 1);
+      response_val = pdMS_TO_TICKS(response_val * 10);
     } else if (header.response_type == RDM_RESPONSE_TYPE_NACK_REASON) {
       // Report the NACK reason
-      rdm_decode_16bit(&rdm->pd, &response->nack_reason, 1);
+      rdm_decode_16bit(&rdm->pd, &response_val, 1);
     } else if (header.response_type == RDM_RESPONSE_TYPE_ACK_OVERFLOW) {
       // This code should never run
-      response->err = ESP_ERR_INVALID_RESPONSE;
+      err = ESP_ERR_INVALID_RESPONSE;
     } else {
       // An unknown response type was received
-      response->err = ESP_ERR_INVALID_RESPONSE;
+      err = ESP_ERR_INVALID_RESPONSE;
+    }
+
+    // Report response back to user
+    if (response != NULL) {
+      response->err = err;
+      response->type = header.response_type;
+      response->num_params = response_val;
     }
   }
 
