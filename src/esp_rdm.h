ea15128bfee16402d3c7ffba09c34409514568cf
Mitch Weisbrod <mitch@theweisbrods.com>
2022-11-11 16:35:29 -0800

    documentation

--- src/esp_rdm.h
+++ src/esp_rdm.h
@@ -25,11 +25,17 @@ extern "C" {
 #define UID2STR(uid) ((uint16_t)(uid >> 32)), ((uint32_t)(uid))
 
 /**
- * @brief // TODO
- * // FIXME: add discovery mute parameters as argument
+ * @brief A callback function type for use with rdm_discover_with_callback().
+ *
+ * @param dmx_num The DMX port number.
+ * @param uid The UID of the found device.
+ * @param device_number The number of the found device.
+ * @param[in] mute_params A pointer to the received mute parameters from the
+ * device.
+ * @param[inout] context A pointer to user-provided context.
  */
 typedef void(rdm_discovery_cb_t)(dmx_port_t dmx_num, rdm_uid_t uid,
-                                 size_t device_index,
+                                 size_t device_num,
                                  rdm_disc_mute_t *mute_params, void *context);
 
 /**
@@ -165,13 +171,13 @@ size_t rdm_discover_with_callback(dmx_port_t dmx_num, rdm_discovery_cb_t cb,
 /**
  * @brief Performes the RDM device discovery algorithm with a default callback
  * function to store the UIDs of found devices in an array.
- * 
+ *
  * @note This discovery algorithm is not recursive like the RDM technical
  * standard suggests, but iterative. It requires the allocation of 784 bytes of
  * data to store a list of discovery requests that must be made. By default,
  * this data is heap allocated but may be stack allocated by configuring
  * settings in the ESP-IDF sdkconfig.
- * 
+ *
  * @param dmx_num The DMX port number.
  * @param[out] uids An array of UIDs used to store found device UIDs.
  * @param size The size of the provided UID array.
@@ -200,7 +206,7 @@ size_t rdm_get_supported_parameters(dmx_port_t dmx_num, rdm_uid_t uid,
 
 /**
  * @brief Sends an RDM DEVICE_INFO request and reads the response, if any.
- * 
+ *
  * @param dmx_num The DMX port number.
  * @param uid The UID to which to address the request.
  * @param sub_device The sub-device to which to address the request.
@@ -251,10 +257,10 @@ size_t rdm_get_dmx_start_address(dmx_port_t dmx_num, rdm_uid_t uid,
 
 /**
  * @brief Sends an RDM DMX_START_ADDRESS SET request.
- * 
+ *
  * @param dmx_num The DMX port number.
  * @param uid The UID to which to address the request.
- * @param sub_device The sub-device to which to address the request. 
+ * @param sub_device The sub-device to which to address the request.
  * @param response A pointer into which to store the RDM response summary.
  * @param start_address The start address to which to set the device or devices.
  * @return true if the request was successful.
@@ -281,7 +287,7 @@ size_t rdm_get_identify_device(dmx_port_t dmx_num, rdm_uid_t uid,
 
 /**
  * @brief Sends an RDM IDENTIFY_DEVICE SET request.
- * 
+ *
  * @param dmx_num The DMX port number.
  * @param uid The UID to which to address the request.
  * @param sub_device The sub-device to which to address the request.
@@ -291,8 +297,8 @@ size_t rdm_get_identify_device(dmx_port_t dmx_num, rdm_uid_t uid,
  * @return false if the request was not successful.
  */
 bool rdm_set_identify_device(dmx_port_t dmx_num, rdm_uid_t uid,
-                               rdm_sub_device_t sub_device,
-                               rdm_response_t *response, bool identify);
+                             rdm_sub_device_t sub_device,
+                             rdm_response_t *response, bool identify);
 
 #ifdef __cplusplus
 }
