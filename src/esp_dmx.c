84e644dda9dc89bd7db90e26c430c0c9c27b9e95
Mitch Weisbrod <mitch@theweisbrods.com>
2022-11-12 07:33:07 -0800

    update packet type on errors

--- src/esp_dmx.c
+++ src/esp_dmx.c
@@ -96,6 +96,9 @@ static void DMX_ISR_ATTR dmx_uart_isr(void *arg) {
           uint8_t *data_ptr = &driver->data.buffer[driver->data.head];
           dmx_uart_read_rxfifo(uart, data_ptr, &read_len);
           driver->data.head += read_len;
+          taskENTER_CRITICAL_ISR(spinlock);
+          driver->data.type = RDM_PACKET_TYPE_NON_RDM;
+          taskEXIT_CRITICAL_ISR(spinlock);
         } else {
           // Data cannot be read into driver buffer
           if (driver->data.head > 0) {
