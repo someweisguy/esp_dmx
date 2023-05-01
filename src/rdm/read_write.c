cdbd4ed64dacf013144bf214a0b6345ba7910409
Mitch Weisbrod <mitch@theweisbrods.com>
2023-05-01 19:01:19 -0400

    minor bitfield optimization

--- src/rdm/read_write.c
+++ src/rdm/read_write.c
@@ -334,7 +334,7 @@ size_t rdm_encode_mute(rdm_mdb_t *mdb, const void *data, int num) {
   if (mdb && data && num) {
     struct rdm_disc_mute_data_t *pd = (void *)mdb->pd;
     const rdm_disc_mute_t *param = data;
-    bzero(mdb->pd, 2);  // FIXME: make the bit field more efficient?
+    pd->control_field = 0;  // TODO: Make the bit field more efficient?
     pd->managed_proxy = param->managed_proxy;
     pd->sub_device = param->sub_device;
     pd->boot_loader = param->boot_loader;
