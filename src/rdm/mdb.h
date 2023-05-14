a8827ad23c58faa9042b5a5b56a77a01e0f020ef
Mitch Weisbrod <mitch@theweisbrods.com>
2023-05-14 16:20:44 -0700

    documentation

--- src/rdm/mdb.h
+++ src/rdm/mdb.h
@@ -20,8 +20,8 @@ extern "C" {
 /**
  * @brief Decodes 8-bit values from an RDM message data block.
  *
- * @param mdb A pointer to a message data block.
- * @param data A pointer to an array in which to store the decoded values.
+ * @param[in] mdb A pointer to a message data block.
+ * @param[out] data A pointer to an array in which to store the decoded values.
  * @param num The number of elements in the array in which to store the decoded
  * values.
  * @return The number of parameters that were decoded.
@@ -30,19 +30,19 @@ int rdm_decode_8bit(const rdm_mdb_t *mdb, void *data, int num);
 
 /**
  * @brief Encodes 8-bit values into an RDM message data block.
- * 
- * @param mdb A pointer to a message data block.
- * @param data A pointer to an array which stores the values to encode.
+ *
+ * @param[out] mdb A pointer to a message data block.
+ * @param[in] data A pointer to an array which stores the values to encode.
  * @param num The number of elements in the array to encode.
- * @return The number of bytes that were encoded. 
+ * @return The number of bytes that were encoded.
  */
 size_t rdm_encode_8bit(rdm_mdb_t *mdb, const void *data, int num);
 
 /**
  * @brief Decodes 16-bit values from an RDM message data block.
  *
- * @param mdb A pointer to a message data block.
- * @param data A pointer to an array in which to store the decoded values.
+ * @param[in] mdb A pointer to a message data block.
+ * @param[out] data A pointer to an array in which to store the decoded values.
  * @param num The number of elements in the array in which to store the decoded
  * values.
  * @return The number of parameters that were decoded.
@@ -51,11 +51,11 @@ int rdm_decode_16bit(const rdm_mdb_t *mdb, void *data, int num);
 
 /**
  * @brief Encodes 16-bit values into an RDM message data block.
- * 
- * @param mdb A pointer to a message data block.
- * @param data A pointer to an array which stores the values to encode.
+ *
+ * @param[out] mdb A pointer to a message data block.
+ * @param[in] data A pointer to an array which stores the values to encode.
  * @param num The number of elements in the array to encode.
- * @return The number of bytes that were encoded. 
+ * @return The number of bytes that were encoded.
  */
 size_t rdm_encode_16bit(rdm_mdb_t *mdb, const void *data, int num);
 
@@ -66,8 +66,8 @@ size_t rdm_encode_16bit(rdm_mdb_t *mdb, const void *data, int num);
 /**
  * @brief Decodes UIDs from an RDM message data block.
  *
- * @param mdb A pointer to a message data block.
- * @param data A pointer to an array in which to store the decoded values.
+ * @param[in] mdb A pointer to a message data block.
+ * @param[out] data A pointer to an array in which to store the decoded values.
  * @param num The number of elements in the array in which to store the decoded
  * values.
  * @return The number of parameters that were decoded.
@@ -76,11 +76,11 @@ int rdm_decode_uids(const rdm_mdb_t *mdb, void *data, int num);
 
 /**
  * @brief Encodes UIDs into an RDM message data block.
- * 
- * @param mdb A pointer to a message data block.
- * @param data A pointer to an array which stores the values to encode.
+ *
+ * @param[out] mdb A pointer to a message data block.
+ * @param[in] data A pointer to an array which stores the values to encode.
  * @param num The number of elements in the array to encode.
- * @return The number of bytes that were encoded. 
+ * @return The number of bytes that were encoded.
  */
 size_t rdm_encode_uids(rdm_mdb_t *mdb, const void *data, int num);
 
@@ -92,8 +92,8 @@ size_t rdm_encode_uids(rdm_mdb_t *mdb, const void *data, int num);
  * @brief Decodes a string from an RDM message data block. Strings may be up to
  * 32 characters long.
  *
- * @param mdb A pointer to a message data block.
- * @param data A pointer to an array in which to store the decoded string.
+ * @param[in] mdb A pointer to a message data block.
+ * @param[out] data A pointer to an array in which to store the decoded string.
  * @param num The number of elements in the array in which to store the decoded
  * string.
  * @return The number of bytes that were decoded, including a null terminator.
@@ -104,8 +104,8 @@ int rdm_decode_string(const rdm_mdb_t *mdb, void *data, int num);
  * @brief Encodes a string into an RDM message data block. Strings may be up to
  * 32 characters long.
  *
- * @param mdb A pointer to a message data block.
- * @param data A pointer to an array which stores the encoded string.
+ * @param[out] mdb A pointer to a message data block.
+ * @param[in] data A pointer to an array which stores the encoded string.
  * @param num The number of elements in the array which stores the string to
  * encode.
  * @return The number of bytes that were decoded, not including a null
@@ -117,8 +117,8 @@ size_t rdm_encode_string(rdm_mdb_t *mdb, const void *data, int num);
 
 /**
  * @brief Encodes a 16-bit NACK reason into an RDM message data block.
- * 
- * @param mdb A pointer to a message data block.
+ *
+ * @param[out] mdb A pointer to a message data block.
  * @param nack_reason A NACK reason to encode.
  * @return The number of bytes that were encoded.
  */
@@ -126,10 +126,10 @@ size_t rdm_encode_nack_reason(rdm_mdb_t *mdb, rdm_nr_t nack_reason);
 
 /**
  * @brief Decodes an RDM mute parameter from a message data block.
- * 
- * @param mdb A pointer to a message data block.
- * @param data A pointer to a buffer which stores the decoded mute parameter,
- * typically an rdm_disc_mute_t.
+ *
+ * @param[in] mdb A pointer to a message data block.
+ * @param[out] data A pointer to a buffer which stores the decoded mute
+ * parameter, typically an rdm_disc_mute_t.
  * @param num The number of elements in the buffer in which to store the decoded
  * mute parameter.
  * @return The number of parameters that were decoded.
@@ -139,9 +139,9 @@ int rdm_decode_mute(const rdm_mdb_t *mdb, void *data, int num);
 /**
  * @brief Encodes an RDM mute parameter into a message data block.
  *
- * @param mdb A pointer to a message data block.
- * @param data A pointer to a buffer which stores the mute parameter to encode,
- * typically an rdm_disc_mute_t.
+ * @param[out] mdb A pointer to a message data block.
+ * @param[in] data A pointer to a buffer which stores the mute parameter to
+ * encode, typically an rdm_disc_mute_t.
  * @param num The number of elements in the buffer in which the mute parameter
  * to encode is stored.
  * @return The number of bytes encoded.
@@ -151,8 +151,8 @@ size_t rdm_encode_mute(rdm_mdb_t *mdb, const void *data, int num);
 /**
  * @brief Decodes an RDM device info parameter from a message data block.
  *
- * @param mdb A pointer to a message data block.
- * @param data A pointer to a buffer which stores the decoded device info
+ * @param[in] mdb A pointer to a message data block.
+ * @param[out] data A pointer to a buffer which stores the decoded device info
  * parameter, typically an rdm_device_info_t.
  * @param num The number of elements in the buffer in which to store the decoded
  * mute parameter.
@@ -163,9 +163,9 @@ int rdm_decode_device_info(const rdm_mdb_t *mdb, void *data, int num);
 /**
  * @brief Encodes an RDM device info parameter into a message data block.
  *
- * @param mdb A pointer to a message data block.
- * @param data A pointer to a buffer which stores the device info parameter to
- * encode, typically an rdm_device_info_t.
+ * @param[out] mdb A pointer to a message data block.
+ * @param[in] data A pointer to a buffer which stores the device info parameter
+ * to encode, typically an rdm_device_info_t.
  * @param num The number of elements in the buffer in which the device
  * info parameter to encode is stored.
  * @return The number of bytes encoded.
