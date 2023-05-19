da04acc23bebbedac5078957edc7fb629e5844ae
Mitch Weisbrod <mitch@theweisbrods.com>
2023-05-18 20:36:34 -0700

    update example

--- examples/idf_rdm_write/idf_rdm_write.c
+++ examples/idf_rdm_write/idf_rdm_write.c
@@ -1,8 +1,12 @@
 /*
 
-  ESP-IDF RDM Write
+  ESP-IDF RDM Discovery
 
-  // TODO:
+  TODO
+
+  Discovery can take several seconds to complete, especially when there are
+  several responder devices on the RDM network. In this example, the amount of
+  time that discovery takes is measured and logged to the terminal.
 
   Note: this example is for use with the ESP-IDF. It will not work on Arduino!
 
@@ -24,52 +28,42 @@
 
 static const char *TAG = "main";
 
-void discover_cb(dmx_port_t dmx_num, rdm_uid_t uid, size_t device_num,
-                 rdm_disc_mute_t *mute_params, void *context) {
-  /* This is a callback which is called during RDM discovery. It can be used to
-    fetch additional device info when a device is discovered. It can also be
-    used to increment a progress bar. In this example, we will just log that a
-    new device has been found. The function rdm_discover_devices_simple() will
-    not execute this callback. */
-  ESP_LOGI(TAG, "Device %i has UID " UIDSTR, device_num, UID2STR(uid));
-}
-
 void app_main() {
   const dmx_port_t dmx_num = DMX_NUM_2;
   ESP_ERROR_CHECK(dmx_set_pin(dmx_num, TX_PIN, RX_PIN, EN_PIN));
   ESP_ERROR_CHECK(dmx_driver_install(dmx_num, DMX_DEFAULT_INTR_FLAGS));
 
-  /* RDM Discovery can take several seconds to complete. With just 1 RDM capable
-    device in the RDM network, discovery should take around 30ms. */
-
+  // Discover devices on the RDM network
   rdm_uid_t uids[32];
-  const int64_t start = esp_timer_get_time();
+  int devices_found = rdm_discover_devices_simple(dmx_num, uids, 32);
 
-  /* Call the default discovery implementation. This implementation searches for
-    RDM devices on the network. When a device is found, its UID is added to an
-    array of UIDs. This function will never overflow the UID array as long as
-    the size argument is set properly. */
-  size_t devices_found = rdm_discover_devices_simple(dmx_num, uids, 32);
-
-  const int64_t stop = esp_timer_get_time();
-
-  // Log how long discovery took and iterate through each UID
-  ESP_LOGI(TAG, "Discovery took %.01fms and found %i device(s).",
-           (stop - start) / 1000.0, devices_found);
   if (devices_found) {
-    // Print the UID of each device found
+    
+    // Iterate through each device to send it a GET DEVICE_INFO request.
     for (int i = 0; i < devices_found; ++i) {
-      ESP_LOGI(TAG, "Device %i has UID " UIDSTR, i, UID2STR(uids[i]));
+      rdm_header_t header = {
+        .dest_uid = uids[i],
+        .sub_device = RDM_SUB_DEVICE_ROOT
+      };
+      rdm_ack_t ack;
+      rdm_device_info_t device_info;
+      rdm_get_device_info(dmx_num, &header, &ack, &device_info);
+
+      // Log the result
+      if (ack.type == RDM_RESPONSE_TYPE_ACK) {
+        ESP_LOGI(TAG,
+                 "Device " UIDSTR
+                 " has a DMX address of %i and a footprint of size %i.",
+                 UID2STR(uids[i]), device_info.start_address,
+                 device_info.footprint);
+
+      } else {
+        ESP_LOGI(TAG, "Unable to get device info for " UIDSTR,
+                 UID2STR(uids[i]));
+      }
     }
   } else {
+    // No devices were found during RDM discovery
     ESP_LOGE(TAG, "Could not find any RDM capable devices.");
   }
-
-  ESP_LOGI(TAG, "Starting discovery with callback...");
-
-  /* Call discovery with a callback. This discovery implementation is exactly
-    the same as rdm_discover_devices_simple() except that when a new device is
-    discovered, a user-specified callback is called. */
-  devices_found = rdm_discover_with_callback(dmx_num, discover_cb, NULL);
-  ESP_LOGI(TAG, "Discovery completed and found %i device(s).", devices_found);
 }
