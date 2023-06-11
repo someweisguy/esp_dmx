2ea8168657d6ef9c9591dcaafb291dac5477ce88
Mitch Weisbrod <mitch@theweisbrods.com>
2023-06-11 10:25:01 -0700

    add TODOs

--- src/rdm/agent.h
+++ src/rdm/agent.h
@@ -166,20 +166,27 @@ size_t rdm_send(dmx_port_t dmx_num, rdm_header_t *header,
                 const rdm_encode_t *encode, rdm_decode_t *decode,
                 rdm_ack_t *ack);
 
+// TODO: docs
 bool rdm_register_disc_unique_branch(dmx_port_t dmx_num, void *context);
 
+// TODO: docs
 bool rdm_register_disc_mute(dmx_port_t dmx_num, void *context);
 
+// TODO: docs
 bool rdm_register_disc_un_mute(dmx_port_t dmx_num, void *context);
 
+// TODO: docs
 bool rdm_register_device_info(dmx_port_t dmx_num,
                               rdm_device_info_t *device_info);
 
+// TODO: docs
 bool rdm_register_software_version_label(dmx_port_t dmx_num,
                                          const char *software_version_label);
 
+// TODO: docs
 bool rdm_register_identify_device(dmx_port_t dmx_num);
 
+// TODO: docs
 bool rdm_register_dmx_start_address(dmx_port_t dmx_num,
                                     uint16_t *dmx_start_address);
 
