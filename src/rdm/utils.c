5031a6a3967dfac5a73cf20ec66cda6c8cf44c1a
Mitch Weisbrod <mitch@theweisbrods.com>
2023-06-11 21:45:58 -0700

    prototype general encode/decode

--- src/rdm/utils.c
+++ src/rdm/utils.c
@@ -49,6 +49,52 @@ inline bool uid_is_target(const rdm_uid_t *uid, const rdm_uid_t *alias) {
          uid_is_eq(uid, alias);
 }
 
+size_t rdm_encode(rdm_mdb_t *mdb, const char *format, const void *pd,
+                  size_t pdl) {
+  size_t written = 0;
+  bool param_is_optional = false;
+  while (written < pdl) {
+    for (const char *f = format; *f != '\0' && written < pdl; ++f) {
+      if (*f == '?') {
+        param_is_optional = true;  // TODO
+        continue;
+      } else if (*f == 'b') {
+        // 8-bit
+        *(uint8_t *)(&mdb->pd[written]) = *(uint8_t *)(pd + written);
+        written += sizeof(uint8_t);
+      } else if (*f == 'w') {
+        // 16-bit
+        *(uint16_t *)(&mdb->pd[written]) = bswap16(*(uint16_t *)(pd + written));
+        written += sizeof(uint16_t);
+      } else if (*f == 'd') {
+        // 32-bit
+        *(uint32_t *)(&mdb->pd[written]) = bswap32(*(uint32_t *)(pd + written));
+        written += sizeof(uint32_t);
+      } else if (*f == 'u') {
+        // 48-bit UID
+        uidmove((rdm_uid_t *)(&mdb->pd[written]), (rdm_uid_t *)(pd + written));
+        written += sizeof(rdm_uid_t);
+      } else if (*f == 'a') {
+        const size_t max_len = (pdl - written) < 32 ? (pdl - written) : 32;
+        size_t len = strnlen((char *)(pd + written), max_len);
+        memmove(&mdb->pd[written], pd + written, len);  // Strip terminator
+        written += len;
+      } // TODO: encode literals
+      param_is_optional = false;
+    }
+  }
+  mdb->pdl = written;
+  return written;
+}
+
+int rdm_decode(const rdm_mdb_t *mdb, const char *format, void *pd, int num) {
+  int decoded = 0;
+
+
+
+  return decoded;
+}
+
 size_t get_preamble_len(const void *data) {
   size_t preamble_len = 0;
   for (const uint8_t *d = data; preamble_len <= 7; ++preamble_len) {
