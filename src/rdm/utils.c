3198268fc7f6f313f31e60a6c4cf2cc099672544
Mitch Weisbrod <mitch@theweisbrods.com>
2023-06-12 14:14:40 -0700

    rough in int literals

--- src/rdm/utils.c
+++ src/rdm/utils.c
@@ -1,5 +1,7 @@
 #include "utils.h"
 
+#include <string.h>
+
 #include "rdm/types.h"
 #include "endian.h"
 
@@ -56,35 +58,55 @@ inline bool uid_is_target(const rdm_uid_t *uid, const rdm_uid_t *alias) {
 size_t rdm_encode(rdm_mdb_t *mdb, const char *format, const void *pd,
                   size_t pdl) {
   size_t written = 0;
-  bool param_is_optional = false;
-  while (written < pdl) {
-    for (const char *f = format; *f != '\0' && written < pdl; ++f) {
-      if (*f == '?') {
-        param_is_optional = true;  // TODO
-        continue;
+  size_t pd_index = 0;
+
+  // TODO: syntax check - literals: #[0-9a-zA-Z]h
+  // TODO: syntax check - 'v' may only be at end of format
+  // TODO: syntax check - 'a' may only be at end of format
+
+  while (pd_index < pdl) {
+    for (const char *f = format; *f != '\0' && pd_index < pdl; ++f) {
+      if (*f == '#') {
+        // Integer literal
+        char *end_ptr;
+        uint64_t literal = strtol(f, &end_ptr, 16);
+        const int num_bytes = ((end_ptr - f) / 2) + ((end_ptr - f) % 2);
+        for (int i = 0; i < num_bytes; ++i) {
+          *(uint8_t *)(&mdb->pd[written + i]) = *((&literal) - (num_bytes - i));
+        }
+        f = end_ptr;
+        written += num_bytes;
       } else if (*f == 'b') {
         // 8-bit
-        *(uint8_t *)(&mdb->pd[written]) = *(uint8_t *)(pd + written);
+        *(uint8_t *)(&mdb->pd[written]) = *(uint8_t *)(pd + pd_index);
         written += sizeof(uint8_t);
+        pd_index += sizeof(uint8_t);
       } else if (*f == 'w') {
         // 16-bit
-        *(uint16_t *)(&mdb->pd[written]) = bswap16(*(uint16_t *)(pd + written));
+        *(uint16_t *)(&mdb->pd[written]) = bswap16(*(uint16_t *)(pd + pd_index));
         written += sizeof(uint16_t);
+        pd_index += sizeof(uint16_t);
       } else if (*f == 'd') {
         // 32-bit
-        *(uint32_t *)(&mdb->pd[written]) = bswap32(*(uint32_t *)(pd + written));
+        *(uint32_t *)(&mdb->pd[written]) = bswap32(*(uint32_t *)(pd + pd_index));
         written += sizeof(uint32_t);
-      } else if (*f == 'u') {
+        pd_index += sizeof(uint32_t);
+      } else if (*f == 'u' || *f == 'v') {
         // 48-bit UID
-        uidmove((rdm_uid_t *)(&mdb->pd[written]), (rdm_uid_t *)(pd + written));
+        if (*f == 'v' && uid_is_null((rdm_uid_t *)(pd + pd_index))) {
+          pd_index += sizeof(rdm_uid_t);
+          continue;
+        }
+        uidmove((rdm_uid_t *)(&mdb->pd[written]), (rdm_uid_t *)(pd + pd_index));
         written += sizeof(rdm_uid_t);
+        pd_index += sizeof(rdm_uid_t);
       } else if (*f == 'a') {
         const size_t max_len = (pdl - written) < 32 ? (pdl - written) : 32;
-        size_t len = strnlen((char *)(pd + written), max_len);
-        memmove(&mdb->pd[written], pd + written, len);  // Strip terminator
+        size_t len = strnlen((char *)(pd + pd_index), max_len);
+        memmove(&mdb->pd[written], pd + pd_index, len);  // Strip terminator
         written += len;
+        pd_index += len;
       } // TODO: encode literals
-      param_is_optional = false;
     }
   }
   mdb->pdl = written;
