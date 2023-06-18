2f36e2893c16d56c3e993f4491eb34fbc1a99f09
Mitch Weisbrod <mitch@theweisbrods.com>
2023-06-17 22:57:55 -0700

    remove func prototype

--- src/rdm/agent.h
+++ src/rdm/agent.h
@@ -24,16 +24,6 @@ extern "C" {
 void rdm_driver_get_uid(dmx_port_t dmx_num, rdm_uid_t *uid);
 
 /**
- * @brief Set the device UID to a custom value. Setting the UID to 0 will reset
- * the UID to its default value.
- *
- * @param dmx_num The DMX port number.
- * @param uid The custom value to which to set the device UID. Must be less than
- * or equal to RDM_MAX_UID.
- */
-void rdm_driver_set_uid(dmx_port_t dmx_num, rdm_uid_t uid);
-
-/**
  * @brief Returns true if RDM discovery responses are be muted on this device.
  *
  * @param dmx_num The DMX port number.
