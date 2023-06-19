9624c54c020d90322930d5dba3cbe406ca442cc1
Mitch Weisbrod <mitch@theweisbrods.com>
2023-06-19 09:00:28 -0700

    rename functions

--- src/rdm/agent.c
+++ src/rdm/agent.c
@@ -10,45 +10,31 @@ static const char *TAG = "rdm_agent";
 extern dmx_driver_t *dmx_driver[DMX_NUM_MAX];
 extern spinlock_t dmx_spinlock[DMX_NUM_MAX];
 
-void rdm_driver_set_device_info(dmx_port_t dmx_num,
-                                const rdm_device_info_t *device_info) {
-  DMX_CHECK(dmx_num < DMX_NUM_MAX, , "dmx_num error");
-  DMX_CHECK(dmx_driver_is_installed(dmx_num), , "driver is not installed");
-  DMX_CHECK(device_info != NULL, , "device_info is null");
-
-  spinlock_t *const restrict spinlock = &dmx_spinlock[dmx_num];
-  dmx_driver_t *const driver = dmx_driver[dmx_num];
-
-  taskENTER_CRITICAL(spinlock);
-  driver->rdm.device_info = *device_info;
-  taskEXIT_CRITICAL(spinlock);
-}
-
-int rdm_driver_get_dmx_start_address(dmx_port_t dmx_num) {
+uint16_t rdm_get_dmx_start_address(dmx_port_t dmx_num) {
   DMX_CHECK(dmx_num < DMX_NUM_MAX, 0, "dmx_num error");
   DMX_CHECK(dmx_driver_is_installed(dmx_num), 0, "driver is not installed");
 
   spinlock_t *const restrict spinlock = &dmx_spinlock[dmx_num];
   dmx_driver_t *const driver = dmx_driver[dmx_num];
 
+  uint16_t dmx_start_address;
   taskENTER_CRITICAL(spinlock);
-  int start_address = driver->rdm.device_info.start_address;
+  dmx_start_address = driver->rdm.device_info.start_address;
   taskEXIT_CRITICAL(spinlock);
 
-  return start_address;
+  return dmx_start_address;
 }
 
-void rdm_driver_set_dmx_start_address(dmx_port_t dmx_num, int start_address) {
+void rdm_set_dmx_start_address(dmx_port_t dmx_num, uint16_t dmx_start_address) {
   DMX_CHECK(dmx_num < DMX_NUM_MAX, , "dmx_num error");
   DMX_CHECK(dmx_driver_is_installed(dmx_num), , "driver is not installed");
-  DMX_CHECK(start_address >= 1 && start_address <= 512, ,
-            "start_address is invalid");
+  DMX_CHECK(dmx_start_address >= 1 && dmx_start_address <= 512, ,
+            "dmx_start_address is invalid");
 
   spinlock_t *const restrict spinlock = &dmx_spinlock[dmx_num];
   dmx_driver_t *const driver = dmx_driver[dmx_num];
 
   taskENTER_CRITICAL(spinlock);
-  driver->rdm.device_info.start_address = start_address;
+  driver->rdm.device_info.start_address = dmx_start_address;
   taskEXIT_CRITICAL(spinlock);
 }
-
--- src/rdm/agent.h
+++ src/rdm/agent.h
@@ -15,23 +15,13 @@ extern "C" {
 #endif
 
 /**
- * @brief Sets the device info for this device.
- *
- * @param dmx_num The DMX port number.
- * @param[in] device_info A pointer to a struct which stores the device info to
- * copy to this device.
- */
-void rdm_driver_set_device_info(dmx_port_t dmx_num,
-                                const rdm_device_info_t *device_info);
-
-/**
  * @brief Gets the DMX start address of this device. To get the DMX start
  * address of a responder device, use `rdm_get_dmx_start_address()`.
  *
  * @param dmx_num The DMX port number.
  * @return The DMX start address of this device or 0 on failure.
  */
-int rdm_driver_get_dmx_start_address(dmx_port_t dmx_num);
+uint16_t rdm_get_dmx_start_address(dmx_port_t dmx_num);
 
 /**
  * @brief Sets the DMX start address of this device. To set the DMX start
@@ -41,7 +31,7 @@ int rdm_driver_get_dmx_start_address(dmx_port_t dmx_num);
  * @param start_address The DMX start address to which to set this device. Must
  * be between 1 and 512 (inclusive).
  */
-void rdm_driver_set_dmx_start_address(dmx_port_t dmx_num, int start_address);
+void rdm_set_dmx_start_address(dmx_port_t dmx_num, uint16_t dmx_start_address);
 
 #ifdef __cplusplus
 }
