1d5b9735bf7561e25f447380d6eeb6d54c152fe1
Mitch Weisbrod <mitch@theweisbrods.com>
2023-07-16 13:58:42 -0700

    todos

--- src/dmx/hal/timer.h
+++ src/dmx/hal/timer.h
@@ -6,19 +6,26 @@
 extern "C" {
 #endif
 
+// TODO: docs
 typedef struct dmx_timer_t *dmx_timer_t;
 
+// TODO: docs
 dmx_timer_t dmx_timer_init(dmx_port_t dmx_num, void *isr_handle,
-                            void *isr_context, int isr_flags);
+                           void *isr_context, int isr_flags);
 
+// TODO: docs
 void dmx_timer_deinit(dmx_timer_t timer);
 
+// TODO: docs
 void dmx_timer_stop(dmx_timer_t timer);
 
+// TODO: docs
 void dmx_timer_set_counter(dmx_timer_t timer, uint64_t counter);
 
+// TODO: docs
 void dmx_timer_set_alarm(dmx_timer_t timer, uint64_t alarm);
 
+// TODO: docs
 void dmx_timer_start(dmx_timer_t timer);
 
 #ifdef __cplusplus
