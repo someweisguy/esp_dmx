25b62eb53249633abbe5f7240bc37ce447bda4d9
Mitch Weisbrod <mitch@theweisbrods.com>
2023-08-19 14:38:34 -0700

    don't auto_reload on ESP_IDF v5+

--- src/dmx/timer.c
+++ src/dmx/timer.c
@@ -100,7 +100,7 @@ void DMX_ISR_ATTR dmx_timer_set_alarm(dmx_timer_handle_t timer,
   const gptimer_alarm_config_t alarm_config = {
       .alarm_count = alarm,
       .reload_count = 0,
-      .flags.auto_reload_on_alarm = true};
+      .flags.auto_reload_on_alarm = false};
   gptimer_set_alarm_action(timer->gptimer_handle, &alarm_config);
 #else
   timer_group_set_alarm_value_in_isr(timer->group, timer->idx, alarm);
