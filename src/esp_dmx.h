2a87dc08c51fad54b351778ab5cc3f931ba80cf8
Arne <arne@rnae.de>
2023-08-25 16:13:37 +0200

    feat: implement dmx_get_device_label

--- src/esp_dmx.h
+++ src/esp_dmx.h
@@ -143,6 +143,17 @@ size_t dmx_get_footprint(dmx_port_t dmx_num, uint8_t personality_num);
 const char *dmx_get_personality_description(dmx_port_t dmx_num,
                                             uint8_t personality_num);
 
+/** 
+ * @brief Gets the device label.
+ * 
+ * @param dmx_num The DMX port number.
+ * @param[out] label A pointer to a buffer that the device_label will be copied into.
+ *                   This will not contain a trailing '\0'
+ * @param labelLen The size of @p label
+ * @return The number of bytes copied
+*/
+size_t dmx_get_device_label(dmx_port_t dmx_num, char * label, size_t label_len);
+
 /**
  * @brief Gets the DMX start address of the DMX driver.
  *
