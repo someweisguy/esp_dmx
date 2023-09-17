29cde9720db66571b36a498b74d58eee206a642e
Arne <arne@rnae.de>
2023-09-17 15:28:10 +0200

    rename dmx_get_device_label -> rdm_get_device_label

--- src/rdm/parameters.h
+++ src/rdm/parameters.h
@@ -83,6 +83,17 @@ bool rdm_get_dmx_start_address(dmx_port_t dmx_num, uint16_t *dmx_start_address);
 bool rdm_set_dmx_start_address(dmx_port_t dmx_num,
                                const uint16_t dmx_start_address);
 
+/** 
+ * @brief Gets the device label.
+ * 
+ * @param dmx_num The DMX port number.
+ * @param[out] label A pointer to a buffer that the device_label will be copied into.
+ *                   This will not contain a trailing '\0'
+ * @param labelLen The size of @p label
+ * @return The number of bytes copied
+*/
+size_t rdm_get_device_label(dmx_port_t dmx_num, char * label, size_t label_len);
+
 #ifdef __cplusplus
 }
 #endif
