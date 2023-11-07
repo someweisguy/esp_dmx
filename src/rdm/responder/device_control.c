a1c08d4da8b2b09c80eb7a83e83ae20aafbfa3e7
Mitch Weisbrod <mitch@theweisbrods.com>
2023-11-07 14:41:18 -0800

    code fixes

--- src/rdm/responder/device_control.c
+++ src/rdm/responder/device_control.c
@@ -2,6 +2,7 @@
 
 #include "dmx/driver.h"
 #include "dmx/struct.h"
+#include "rdm/responder/misc.h"
 
 bool rdm_register_identify_device(dmx_port_t dmx_num, rdm_callback_t cb,
                                   void *context) {
@@ -9,23 +10,18 @@ bool rdm_register_identify_device(dmx_port_t dmx_num, rdm_callback_t cb,
   DMX_CHECK(cb != NULL, false, "cb is null");
   DMX_CHECK(dmx_driver_is_installed(dmx_num), false, "driver is not installed");
 
-  const rdm_pid_description_t pd_def = {.pid = RDM_PID_IDENTIFY_DEVICE,
-                                        .pdl_size = sizeof(uint8_t),
-                                        .data_type = RDM_DS_UNSIGNED_BYTE,
-                                        .cc = RDM_CC_GET_SET,
-                                        .unit = RDM_UNITS_NONE,
-                                        .prefix = RDM_PREFIX_NONE,
-                                        .min_value = 0,
-                                        .max_value = 1,
-                                        .default_value = 0,
-                                        .description = "Identify Device"};
-  const char *format = "b$";
-  const bool nvs = false;
+  // Define the parameter
+  const rdm_pid_t pid = RDM_PID_IDENTIFY_DEVICE;
+  const rdm_pd_schema_t schema = {
+      .data_type = RDM_DS_NOT_DEFINED,
+      .cc = RDM_CC_GET_SET,
+      .size = sizeof(uint8_t),
+      .format = "b$",
+      .nvs = false,
+      .response_handler = rdm_response_handler_simple,
+  };
 
-  uint8_t default_value = 0;
-  rdm_pd_add_new(dmx_num, RDM_SUB_DEVICE_ROOT, &pd_def, format, nvs,
-                 rdm_simple_response_cb, &default_value);
-
-  return rdm_pd_update_callback(dmx_num, RDM_SUB_DEVICE_ROOT,
-                                RDM_PID_IDENTIFY_DEVICE, cb, context);
+  const uint8_t init_value = 0;
+  rdm_pd_add_new(dmx_num, RDM_SUB_DEVICE_ROOT, pid, &schema, NULL, &init_value);
+  return rdm_pd_update_callback(dmx_num, RDM_SUB_DEVICE_ROOT, pid, cb, context);
 }
