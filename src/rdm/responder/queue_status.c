371deb9f27e6fa4f2d00faa2dc6218d7835be1d4
Mitch Weisbrod <mitch@theweisbrods.com>
2023-12-14 21:15:02 -0800

    implement queue

--- src/rdm/responder/queue_status.c
+++ src/rdm/responder/queue_status.c
@@ -1,66 +1,77 @@
 #include "include/queue_status.h"
 
 #include "dmx/include/driver.h"
+#include "dmx/include/io.h"
 #include "dmx/include/struct.h"
+#include "rdm/responder/include/utils.h"
 
-// static int rdm_rhd_status_messages(dmx_port_t dmx_num, rdm_header_t *header,
-//                                    void *pd, uint8_t *pdl_out,
-//                                    const rdm_pd_schema_t *schema) {
-//   *pdl_out = 0;  // TODO: implement status messages
-//   return RDM_RESPONSE_TYPE_ACK;
-// }
+static size_t rdm_rhd_queued_message(dmx_port_t dmx_num,
+                                     const rdm_pd_definition_t *definition,
+                                     const rdm_header_t *header) {
+  if (header->sub_device != RDM_SUB_DEVICE_ROOT) {
+    return rdm_write_nack_reason(dmx_num, header,
+                                 RDM_NR_SUB_DEVICE_OUT_OF_RANGE);
+  }
 
-// static int rdm_rhd_queued_message(dmx_port_t dmx_num, rdm_header_t *header,
-//                                   void *pd, uint8_t *pdl_out,
-//                                   const rdm_pd_schema_t *schema) {
-//   // Verify data is valid
-//   const uint8_t status_type_requested = *(uint8_t *)pd;
-//   if (status_type_requested != RDM_STATUS_GET_LAST_MESSAGE &&
-//       status_type_requested != RDM_STATUS_ADVISORY &&
-//       status_type_requested != RDM_STATUS_WARNING &&
-//       status_type_requested != RDM_STATUS_ERROR) {
-//     *pdl_out = rdm_pd_serialize_word(pd, RDM_NR_DATA_OUT_OF_RANGE);
-//     return RDM_RESPONSE_TYPE_NACK_REASON;
-//   }  // TODO: ensure error-checking is correct
+  // Verify status type is valid
+  uint8_t status_type;
+  if (!rdm_read_pd(dmx_num, definition->get.request.format, &status_type,
+                   sizeof(status_type))) {
+    return rdm_write_nack_reason(dmx_num, header, RDM_NR_HARDWARE_FAULT);
+  }
+  if (status_type < definition->min_value ||
+      status_type > definition->max_value) {
+    return rdm_write_nack_reason(dmx_num, header, RDM_NR_DATA_OUT_OF_RANGE);
+  }
 
-//   int ack;
+  // Determine what the response will be depending on the RDM queue size
+  rdm_pid_t pid;
+  rdm_header_t response_header = *header;
+  const rdm_pd_definition_t *response_definition;
+  if (rdm_queue_size(dmx_num) > 0) {
+    pid = rdm_queue_pop(dmx_num);
+    response_definition = rdm_parameter_lookup(pid);
+    assert(response_definition != NULL);
+  } else {
+    pid = RDM_PID_STATUS_MESSAGE;
+    response_definition = rdm_parameter_lookup(pid);
+    if (response_definition == NULL) {
+      response_header.pid = RDM_PID_STATUS_MESSAGE;
+      return rdm_write_ack(dmx_num, &response_header, NULL, NULL, 0);
+    }
+  }
 
-//   // Pop a PID from the queue and attempt to serve the queued data
-//   const rdm_pid_t queue_pid = rdm_queue_pop(dmx_num);
-//   if (queue_pid != 0) {
-//     // Call the response handler for the queued PID
-//     header->pid = queue_pid;
-//     ack = rdm_pd_call_response_handler(dmx_num, header, pd, pdl_out);
-//   } else {
-//     // When there aren't any queued messages respond with a status message
-//     header->pid = RDM_PID_STATUS_MESSAGE;
-//     const rdm_pd_schema_t *new_schema =
-//         rdm_pd_get_schema(dmx_num, header->pid, header->sub_device);
-//     ack = rdm_rhd_status_messages(dmx_num, header, pd, pdl_out, new_schema);
-//   }
-
-//   return ack;
-// }
+  response_header.pid = pid;
+  return response_definition->get.handler(dmx_num, response_definition,
+                                          &response_header);
+}
 
 bool rdm_register_queued_message(dmx_port_t dmx_num, rdm_callback_t cb,
                                  void *context) {
   DMX_CHECK(dmx_num < DMX_NUM_MAX, false, "dmx_num error");
   DMX_CHECK(dmx_driver_is_installed(dmx_num), false, "driver is not installed");
 
-  // // Define the parameter
-  // const rdm_pid_t pid = RDM_PID_QUEUED_MESSAGE;
-  // const rdm_pd_definition_t def = {
-  //     .schema = {.data_type = RDM_DS_NOT_DEFINED,
-  //                .cc = RDM_CC_GET,
-  //                .pdl_size = sizeof(uint8_t),
-  //                .alloc_size = 0,  // Parameter is deterministic
-  //                .format = "b$"},
-  //     .nvs = false,
-  //     .response_handler = rdm_rhd_queued_message,
-  // };
+  // Define the parameter
+  const rdm_pid_t pid = RDM_PID_QUEUED_MESSAGE;
+  static const rdm_pd_definition_t definition = {
+      .pid = pid,
+      .pid_cc = RDM_CC_GET,
+      .ds = RDM_DS_NOT_DEFINED,
+      .get = {.handler = rdm_simple_response_handler,
+              .request.format = NULL,
+              .response.format = "b$"},
+      .set = {.handler = NULL, .request.format = NULL, .response.format = NULL},
+      .pdl_size = sizeof(uint8_t),
+      .max_value = RDM_STATUS_ERROR,
+      .min_value = RDM_STATUS_GET_LAST_MESSAGE,
+      .units = RDM_UNITS_NONE,
+      .prefix = RDM_PREFIX_NONE,
+      .description = NULL};
+  rdm_parameter_define(&definition);
 
-  // rdm_pd_add_deterministic(dmx_num, pid, RDM_SUB_DEVICE_ROOT, &def, NULL);
-  // return rdm_pd_update_callback(dmx_num, pid, RDM_SUB_DEVICE_ROOT, cb, context);
+  // Add the parameter as a NULL static variable
+  const bool nvs = false;
+  rdm_parameter_add_static(dmx_num, RDM_SUB_DEVICE_ROOT, pid, nvs, NULL, 0);
 
   return false;
 }
