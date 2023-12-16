f97fbecbfbd46b01f0f3df63adcf1e455b0c2311
Mitch Weisbrod <mitch@theweisbrods.com>
2023-12-15 20:47:32 -0800

    update macro name

--- src/dmx/include/struct.h
+++ src/dmx/include/struct.h
@@ -65,15 +65,14 @@ extern "C" {
 #define RDM_UID_DEVICE_ID (0xffffffff)
 #endif
 
-#ifdef CONFIG_RDM_RESPONDER_MAX_OPTIONAL_PARAMETERS
+#ifdef CONFIG_RDM_DEFINITION_COUNT_OPTIONAL
 /** @brief The maximum number of optional parameters that the RDM responder can
  * support. This value is editable in the Kconfig.*/
-#define RDM_DEFINITION_COUNT_OPTIONAL \
-  (CONFIG_RDM_RESPONDER_MAX_OPTIONAL_PARAMETERS)
+#define RDM_DEFINITION_COUNT_OPTIONAL (CONFIG_RDM_DEFINITION_COUNT_OPTIONAL)
 #else
 /** @brief The maximum number of optional parameters that the RDM responder can
  * support.*/
-#define RDM_DEFINITION_COUNT_OPTIONAL 25
+#define RDM_DEFINITION_COUNT_OPTIONAL 32
 #endif
 
 #if defined(CONFIG_DMX_ISR_IN_IRAM) || ESP_IDF_VERSION_MAJOR < 5
