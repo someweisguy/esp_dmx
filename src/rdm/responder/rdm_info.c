21bf8a44423f4ca3c763b5b41f9be8b79327a3b7
Mitch Weisbrod <mitch@theweisbrods.com>
2023-12-15 16:16:41 -0800

    implement parameter description

--- src/rdm/responder/rdm_info.c
+++ src/rdm/responder/rdm_info.c
@@ -4,15 +4,11 @@
 #include "dmx/include/struct.h"
 #include "endian.h"
 #include "rdm/responder/include/utils.h"
+#include "dmx/include/io.h"
 
 static size_t rdm_rhd_get_supported_parameters(
     dmx_port_t dmx_num, const rdm_pd_definition_t *definition,
     const rdm_header_t *header) {
-  if (header->sub_device != RDM_SUB_DEVICE_ROOT) {
-    return rdm_write_nack_reason(dmx_num, header,
-                                 RDM_NR_SUB_DEVICE_OUT_OF_RANGE);
-  }
-
   int pid_count = 0;
   uint16_t pids[115];
 
@@ -45,34 +41,49 @@ static size_t rdm_rhd_get_supported_parameters(
                        pdl);
 }
 
-  return rdm_write_ack(dmx_num, header, definition->get.response.format, pids,
-                       pid_count * sizeof(uint16_t));
+static size_t rdm_rhd_get_parameter_description(
+    dmx_port_t dmx_num, const rdm_pd_definition_t *definition,
+    const rdm_header_t *header) {
+  // This request may only be sent to the root device
+  if (header->sub_device != RDM_SUB_DEVICE_ROOT) {
+    return rdm_write_nack_reason(dmx_num, header,
+                                 RDM_NR_SUB_DEVICE_OUT_OF_RANGE);
+  }
+
+  // Read the request data
+  uint16_t pid;
+  if (!rdm_read_pd(dmx_num, definition->get. request.format, &pid,
+                   sizeof(pid))) {
+    return rdm_write_nack_reason(dmx_num, header, RDM_NR_HARDWARE_FAULT);
+  }
+
+  // Ensure the request PID is within bounds
+  if (pid < definition->min_value || pid > definition->max_value) {
+    return rdm_write_nack_reason(dmx_num, header, RDM_NR_DATA_OUT_OF_RANGE);
+  }
+
+  // Ensure the request PID is known
+  const rdm_pd_definition_t *requested_definition = rdm_parameter_lookup(pid);
+  if (requested_definition == NULL) {
+    // Don't return RDM_NR_UNKNOWN_PID because it has a different meaning
+    return rdm_write_nack_reason(dmx_num, header, RDM_NR_DATA_OUT_OF_RANGE);
+  }
+
+  rdm_pid_description_t pd;
+  pd.pid = pid;
+  pd.pdl_size = requested_definition->pdl_size;
+  pd.data_type = requested_definition->ds;
+  pd.cc = requested_definition->pid_cc;
+  pd.unit = requested_definition->units;
+  pd.prefix = requested_definition->prefix;
+  pd.min_value = requested_definition->min_value;
+  pd.max_value = requested_definition->max_value;
+  pd.default_value = requested_definition->default_value;
+  strncpy(pd.description, requested_definition->description, 32);
+
+  return rdm_write_ack(dmx_num, header, definition->get.response.format,
+                       &pd, sizeof(pd));
 }
-
-// static int rdm_rhd_parameter_description(dmx_port_t dmx_num,
-//                                          rdm_header_t *header, void *pd,
-//                                          uint8_t *pdl_out,
-//                                          const rdm_pd_schema_t *schema) {
-//   if (header->sub_device != RDM_SUB_DEVICE_ROOT) {
-//     *pdl_out = rdm_pd_serialize_word(pd, RDM_NR_SUB_DEVICE_OUT_OF_RANGE);
-//     return RDM_RESPONSE_TYPE_NACK_REASON;
-//   }
-
-//   // Get the requested PID and avoid undefined behavior (strict aliasing rule)
-//   uint16_t requested_pid;
-//   rdm_pd_deserialize(&requested_pid, sizeof(requested_pid), "w$", pd);
-
-//   // Get the PID description - fails if no PID or description found
-//   rdm_pid_description_t description;
-//   if (!rdm_pd_get_description(dmx_num, header->sub_device, requested_pid,
-//                               &description)) {
-//     *pdl_out = rdm_pd_serialize_word(pd, RDM_NR_DATA_OUT_OF_RANGE);
-//     return RDM_RESPONSE_TYPE_NACK_REASON;
-//   }
-
-//   *pdl_out = rdm_pd_serialize(pd, 231, schema->format, &description);
-//   return RDM_RESPONSE_TYPE_ACK;
-// }
 
 bool rdm_register_supported_parameters(dmx_port_t dmx_num, rdm_callback_t cb,
                                        void *context) {
@@ -109,21 +120,27 @@ bool rdm_register_parameter_description(dmx_port_t dmx_num, rdm_callback_t cb,
   DMX_CHECK(dmx_num < DMX_NUM_MAX, false, "dmx_num error");
   DMX_CHECK(dmx_driver_is_installed(dmx_num), false, "driver is not installed");
 
-  // // Define the parameter
-  // const rdm_pid_t pid = RDM_PID_PARAMETER_DESCRIPTION;
-  // const rdm_pd_definition_t def = {
-  //     .schema = {.data_type = RDM_DS_BIT_FIELD,
-  //                .cc = RDM_CC_GET,
-  //                .pdl_size = sizeof(uint16_t),
-  //                .alloc_size = 0,  // Parameter is deterministic
-  //                .format = "wbbb#00hbbddda$"},
-  //     .nvs = false,
-  //     .response_handler = rdm_rhd_parameter_description,
-  // };
+  // Define the parameter
+  const rdm_pid_t pid = RDM_PID_PARAMETER_DESCRIPTION;
+  static const rdm_pd_definition_t definition = {
+      .pid = pid,
+      .pid_cc = RDM_CC_GET,
+      .ds = RDM_DS_ASCII,
+      .get = {.handler = rdm_rhd_get_parameter_description,
+              .request.format = "w$",
+              .response.format = "wbbbx00bbddda"},
+      .set = {.handler = NULL, .request.format = NULL, .response.format = NULL},
+      .pdl_size = sizeof(uint16_t),
+      .max_value = RDM_PID_MANUFACTURER_SPECIFIC_END,
+      .min_value = RDM_PID_MANUFACTURER_SPECIFIC_BEGIN,
+      .units = RDM_UNITS_NONE,
+      .prefix = RDM_PREFIX_NONE,
+      .description = NULL};
+  rdm_parameter_define(&definition);
 
-  // rdm_pd_add_deterministic(dmx_num, pid, RDM_SUB_DEVICE_ROOT, &def,
-  //                          rdm_pd_get_description);
-  // return rdm_pd_update_callback(dmx_num, pid, RDM_SUB_DEVICE_ROOT, cb,
-  // context);
-  return false;
+  // Add the parameter as a NULL static variable
+  const bool nvs = false;
+  rdm_parameter_add_static(dmx_num, RDM_SUB_DEVICE_ROOT, pid, nvs, NULL, 0);
+
+  return true;
 }
