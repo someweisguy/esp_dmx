81b570e33a3826c542d848d8a9816b05e4f5f7c0
Mitch Weisbrod <mitch@theweisbrods.com>
2023-12-17 08:14:37 -0800

    formatting

--- src/rdm/responder/device_control.c
+++ src/rdm/responder/device_control.c
@@ -34,7 +34,7 @@ bool rdm_register_identify_device(dmx_port_t dmx_num, rdm_callback_t cb,
   const bool nvs = true;
   const uint8_t init_value = 0;
   if (!rdm_parameter_add_dynamic(dmx_num, RDM_SUB_DEVICE_ROOT, pid, nvs,
-                                &init_value, sizeof(init_value))) {
+                                 &init_value, sizeof(init_value))) {
     return false;
   }
 
@@ -46,8 +46,8 @@ size_t rdm_get_identify_device(dmx_port_t dmx_num, bool *identify) {
   DMX_CHECK(identify != NULL, 0, "identify is null");
   DMX_CHECK(dmx_driver_is_installed(dmx_num), 0, "driver is not installed");
 
-  return rdm_parameter_copy(dmx_num, RDM_SUB_DEVICE_ROOT, RDM_PID_IDENTIFY_DEVICE,
-                    identify, sizeof(uint8_t));
+  return rdm_parameter_copy(dmx_num, RDM_SUB_DEVICE_ROOT,
+                            RDM_PID_IDENTIFY_DEVICE, identify, sizeof(uint8_t));
 }
 
 bool rdm_set_identify_device(dmx_port_t dmx_num, const bool identify) {
@@ -61,6 +61,6 @@ bool rdm_set_identify_device(dmx_port_t dmx_num, const bool identify) {
     return false;
   }
   rdm_queue_push(dmx_num, pid);
-  
+
   return true;
 }
