736873b8e45df5fb12f6d5dca1f4df87646ac28f
Mitch Weisbrod <mitch@theweisbrods.com>
2023-12-20 21:01:42 -0800

    add docs

--- src/dmx/include/parameter.h
+++ src/dmx/include/parameter.h
@@ -13,16 +13,50 @@
 extern "C" {
 #endif
 
-// FIXME
-#define rdm_format_is_valid(f) \
-  ((f) == NULL || dmx_parameter_format_size(f) > 0)
+// TODO: docs
+#define rdm_format_is_valid(f) ((f) == NULL || dmx_parameter_format_size(f) > 0)
 
-// TODO: docs, not thread-safe
+/**
+ * @brief Allocates and adds a parameter to the DMX driver. The parameter is
+ * heap-allocated. This function is not thread-safe.
+ *
+ * Each parameter can be identified using its DMX port number, sub-device
+ * number, and PID. Each DMX port and sub-device may possess many parameters but
+ * each sub-device parameter's PID must be unique.
+ *
+ * @param dmx_num The DMX port number.
+ * @param sub_device The sub-device number.
+ * @param pid The parameter ID which to add.
+ * @param non_volatile True if the parameter should persist after the ESP32 is
+ * power-cycled.
+ * @param[in] init The value to which the parameter should be initialized to or NULL
+ * to memset the parameter to zero.
+ * @param size The size of the parameter to allocate.
+ * @return true if the parameter already existed or was allocated.
+ * @return false if the parameter could not be allocated.
+ */
 bool dmx_parameter_add_dynamic(dmx_port_t dmx_num, rdm_sub_device_t sub_device,
                                rdm_pid_t pid, bool non_volatile,
                                const void *init, size_t size);
 
-// TODO: docs, not thread-safe
+/**
+ * @brief Allocates and adds a parameter to the DMX driver. The parameter is
+ * statically allocated. This function is not thread-safe.
+ *
+ * Each parameter can be identified using its DMX port number, sub-device
+ * number, and PID. Each DMX port and sub-device may possess many parameters but
+ * each sub-device parameter's PID must be unique.
+ *
+ * @param dmx_num The DMX port number.
+ * @param sub_device The sub-device number.
+ * @param pid The parameter ID which to add.
+ * @param non_volatile True if the parameter should persist after the ESP32 is
+ * power-cycled.
+ * @param[in] data A pointer to the memory which stores the parameter data.
+ * @param size The size of the parameter.
+ * @return true if the parameter already existed or was allocated.
+ * @return false if the parameter could not be allocated.
+ */
 bool dmx_parameter_add_static(dmx_port_t dmx_num, rdm_sub_device_t sub_device,
                               rdm_pid_t pid, bool non_volatile, void *data,
                               size_t size);
@@ -30,7 +64,16 @@ bool dmx_parameter_add_static(dmx_port_t dmx_num, rdm_sub_device_t sub_device,
 bool dmx_parameter_add_null(dmx_port_t dmx_num, rdm_sub_device_t sub_device,
                             rdm_pid_t pid);
 
-// TODO: docs
+/**
+ * @brief Returns true if the parameter exists on the given DMX port and
+ * sub-device.
+ *
+ * @param dmx_num The DMX port number. 
+ * @param sub_device The sub-device number.
+ * @param pid The parameter ID.
+ * @return true if the parameter exists.
+ * @return false if the parameter does not exist.
+ */
 bool dmx_parameter_exists(dmx_port_t dmx_num, rdm_sub_device_t sub_device,
                           rdm_pid_t pid);
 
