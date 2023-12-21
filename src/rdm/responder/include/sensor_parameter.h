63a13bcfb6d2206739a277a1f45af4b37f0273cb
Mitch Weisbrod <mitch@theweisbrods.com>
2023-12-21 14:42:11 -0800

    comments

--- src/rdm/responder/include/sensor_parameter.h
+++ src/rdm/responder/include/sensor_parameter.h
@@ -14,16 +14,24 @@
 extern "C" {
 #endif
 
+// TODO: docs
 bool rdm_register_sensor_definition(dmx_port_t dmx_num, uint8_t defintion_count,
                          rdm_callback_t cb, void *context);
 
+// TODO: docs
 bool rdm_register_sensor(dmx_port_t dmx_num, uint8_t sensor_count,
                          rdm_callback_t cb, void *context);
 
+// TODO: docs
 bool rdm_register_record_sensors(dmx_port_t dmx_num, rdm_callback_t cb,
                                  void *context);
 
+// TODO: docs
 uint8_t rdm_sensor_get_count(dmx_port_t dmx_num, rdm_sub_device_t sub_device);
+
+// TODO: docs
+bool rdm_sensor_set(dmx_port_t dmx_num, rdm_sub_device_t sub_device,
+                    uint8_t sensor_num, int16_t value);
 
 #ifdef __cplusplus
 }
