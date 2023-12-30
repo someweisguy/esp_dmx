fb9df61c34bc072f260e9307e64b7c4dc109586d
Mitch Weisbrod <mitch@theweisbrods.com>
2023-12-30 10:17:35 -0800

    docs

--- src/dmx/include/io.h
+++ src/dmx/include/io.h
@@ -66,7 +66,42 @@ bool rdm_read_header(dmx_port_t dmx_num, rdm_header_t *header);
  * @brief Reads RDM parameter data from the DMX driver buffer. This function
  * does not verify that the packet being read is a valid RDM packet.
  *
- * // TODO: documentation for format strings
+ * Parameter fields are written and read using a format string. This provides
+ * instructions on how data is written. It is necessary to use a format string
+ * because RDM requires that fields in a parameter are written and read in big
+ * endian format. The following characters can be used to write parameter data:
+ * - 'b' writes an 8-bit byte of data.
+ * - 'w' writes a 16-bit word of data.
+ * - 'd' writes a 32-bit dword of data.
+ * - 'u' writes a 48-bit UID.
+ * - 'v' writes an optional 48-bit UID if the UID is not 0000:00000000. Optional
+ *   UIDs must be at the end of the format string.
+ * - 'a' writes an ASCII string. ASCII strings may be up to 32 characters long
+ *   and may or may not be null-terminated. An ASCII string must be at the end
+ *   of the format string.
+ *
+ * Integer literals may be written and read by beginning the integer with 'x'
+ * and writing the literal in hexadecimal form. Integer literals must contain 
+ * two hexadecimal digits. For example, 0xAB is represented as "xab". Integer
+ * literals are written into the destination regardless of what the underlying
+ * value is. This is used for situations such as reading or writing an
+ * rdm_device_info_t wherein the first two bytes are 0x01 and 0x00.
+ *
+ * Parameters will continue to be written or read as long as the number of bytes
+ * does not exceed the size of the destination buffer as provided by the num
+ * argument. A single parameter may be written or read instead of multiple by 
+ * including a '$' character at the end of the format string. The 'a' and 'v'
+ * fields must be at the end of each parameter. When reading or writing a
+ * parameter which uses one of these fields, a terminating '$' is not necessary
+ * though it may be used for readability.
+ *
+ * Example format strings and their corresponding PIDs are included below.
+ *
+ * RDM_PID_DISC_UNIQUE_BRANCH: "uu$"
+ * RDM_PID_DISC_MUTE: "wv$" or "wv"
+ * RDM_PID_DEVICE_INFO: "x01x00wwdwbbwwb$"
+ * RDM_PID_SOFTWARE_VERSION_LABEL: "a$" or "a"
+ * RDM_PID_DMX_START_ADDRESS: "w$"
  *
  * @param dmx_num The DMX port number.
  * @param[in] format The format string of the RDM parameter data.
@@ -117,7 +152,42 @@ int dmx_write_slot(dmx_port_t dmx_num, size_t slot_num, uint8_t value);
  * @brief Writes an RDM packet into the DMX driver buffer so it may be sent with
  * dmx_send().
  *
- * // TODO: documentation for format strings
+ * Parameter fields are written and read using a format string. This provides
+ * instructions on how data is written. It is necessary to use a format string
+ * because RDM requires that fields in a parameter are written and read in big
+ * endian format. The following characters can be used to write parameter data:
+ * - 'b' writes an 8-bit byte of data.
+ * - 'w' writes a 16-bit word of data.
+ * - 'd' writes a 32-bit dword of data.
+ * - 'u' writes a 48-bit UID.
+ * - 'v' writes an optional 48-bit UID if the UID is not 0000:00000000. Optional
+ *   UIDs must be at the end of the format string.
+ * - 'a' writes an ASCII string. ASCII strings may be up to 32 characters long
+ *   and may or may not be null-terminated. An ASCII string must be at the end
+ *   of the format string.
+ *
+ * Integer literals may be written and read by beginning the integer with 'x'
+ * and writing the literal in hexadecimal form. Integer literals must contain 
+ * two hexadecimal digits. For example, 0xAB is represented as "xab". Integer
+ * literals are written into the destination regardless of what the underlying
+ * value is. This is used for situations such as reading or writing an
+ * rdm_device_info_t wherein the first two bytes are 0x01 and 0x00.
+ *
+ * Parameters will continue to be written or read as long as the number of bytes
+ * does not exceed the size of the destination buffer as provided by the num
+ * argument. A single parameter may be written or read instead of multiple by 
+ * including a '$' character at the end of the format string. The 'a' and 'v'
+ * fields must be at the end of each parameter. When reading or writing a
+ * parameter which uses one of these fields, a terminating '$' is not necessary
+ * though it may be used for readability.
+ *
+ * Example format strings and their corresponding PIDs are included below.
+ *
+ * RDM_PID_DISC_UNIQUE_BRANCH: "uu$"
+ * RDM_PID_DISC_MUTE: "wv$" or "wv"
+ * RDM_PID_DEVICE_INFO: "x01x00wwdwbbwwb$"
+ * RDM_PID_SOFTWARE_VERSION_LABEL: "a$" or "a"
+ * RDM_PID_DMX_START_ADDRESS: "w$"
  *
  * @param dmx_num The DMX port number.
  * @param[in] header A pointer which stores RDM header information.
