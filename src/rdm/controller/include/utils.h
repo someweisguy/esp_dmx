54481a9c7ce119851228b7fbe7ae289fd63fb10f
Mitch Weisbrod <mitch@theweisbrods.com>
2024-01-04 20:56:25 -0800

    documentation

--- src/rdm/controller/include/utils.h
+++ src/rdm/controller/include/utils.h
@@ -15,12 +15,22 @@
 extern "C" {
 #endif
 
+/*
+
+    size_t pdl;
+
+    TickType_t timer;
+
+    rdm_nr_t nack_reason;
+*/
+
 /**
  * @brief Sends an RDM controller request and processes the response. This
  * function writes, sends, receives, and reads a request and response RDM
  * packet. It performs error checking on the written packet to ensure that it
- * adheres to RDM specification and prevents RDM bus errors. An rdm_ack_t is
- * provided to process DMX and RDM errors.
+ * adheres to RDM specification and prevents RDM bus errors. Any parameter data
+ * received in the RDM response must be read by using rdm_read_pd(). An
+ * rdm_ack_t is provided to process DMX and RDM errors.
  * - ack.err will evaluate to true if an error occurred during the sending or
  *   receiving of raw DMX data. RDM data will not be processed if an error
  *   occurred. If a response was expected but none was received, ack.err will
@@ -39,32 +49,44 @@ extern "C" {
  *   ack.type will be set to the ack type received in the RDM response.
  * - ack.message_count indicates the number of messages that are waiting to be
  *   retrieved from the responder's message queue.
- * - ack.timer and ack.nack_reason are a union which should be read depending on
- *   the value of ack.type. If ack.type is RDM_RESPONSE_TYPE_ACK_TIMER,
- *   ack.timer should be read. ack.timer is the estimated amount of time in
- *   FreeRTOS ticks until the responder is able to provide a response to the
- *   request. If ack.type is RDM_RESPONSE_TYPE_NACK_REASON, ack.nack_reason
- *   should be read to get the NACK reason.
+ * - ack.pdl, ack.timer and ack.nack_reason are a union which should be read
+ * depending on the value of ack.type. If ack.type is RDM_RESPONSE_TYPE_ACK,
+ * ack.pdl should be read. ack.pdl is the parameter data length of the RDM
+ * response. If ack.type is RDM_RESPONSE_TYPE_ACK_TIMER, ack.timer should be
+ * read. ack.timer is the estimated amount of time in FreeRTOS ticks until the
+ * responder is able to provide a response to the request. If ack.type is
+ * RDM_RESPONSE_TYPE_NACK_REASON, ack.nack_reason should be read to get the NACK
+ * reason.
  *
  * @param dmx_num The DMX port number.
- * @param[inout] header A pointer which stores header information for the RDM
- * request.
- * @param[in] pd_in A pointer which stores parameter data to be written.
- * @param[out] pd_out A pointer which stores parameter data which was read, if a
- * response was received. This can be an alias of pd_in.
- * @param[inout] pdl The size of the pd_out buffer. When receiving data, this is
- * set to the PDL of the received data. Used to prevent buffer overflows.
+ * @param[in] dest_uid A pointer to the UID of the destination.
+ * @param sub_device The sub-device number of the destination.
+ * @param pid The parameter ID of the request.
+ * @param cc The command class of the request.
+ * @param[in] format The RDM parameter format string. More information can about
+ * RDM parameter format strings by reading the documentation on the
+ * rdm_read_pd() and rdm_write() functions.
+ * @param[in] pd A pointer which stores parameter data to be included with the
+ * RDM request.
+ * @param pdl The size of the parameter data.
  * @param[out] ack A pointer to an rdm_ack_t which stores information about the
  * RDM response.
- * @return true if an RDM_RESPONSE_TYPE_ACK response was received.
- * @return false if any other response type was received.
- */ // TODO: docs
+ * @return The number of bytes that were received in the response parameter
+ * data.
+ */
 size_t rdm_send_generic(dmx_port_t dmx_num, const rdm_uid_t *dest_uid,
                         rdm_sub_device_t sub_device, rdm_pid_t pid, rdm_cc_t cc,
                         const char *format, const void *pd, size_t pdl,
                         rdm_ack_t *ack);
 
-// TODO: docs
+/**
+ * @brief Get the transaction number of the RDM controller. This number is
+ * included in every RDM controller request. It is incremented after every RDM
+ * message is sent.
+ *
+ * @param dmx_num The DMX port number.
+ * @return The current RDM transaction number.
+ */
 size_t rdm_get_transaction_num(dmx_port_t dmx_num);
 
 #ifdef __cplusplus
