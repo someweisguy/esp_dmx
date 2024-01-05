9f935bf8994ba23f607c5eb3c4fb8e003db76b19
Mitch Weisbrod <mitch@theweisbrods.com>
2024-01-04 18:53:24 -0800

    docs

--- src/rdm/responder.h
+++ src/rdm/responder.h
@@ -13,8 +13,14 @@ extern "C" {
 #endif
 
 /**
- * @brief The function type for user callbacks in RDM responses.
- */ // TODO: better docs
+ * @brief The function type used for callbacks after receiving an RDM request
+ * and sending a response.
+ * 
+ * @param dmx_num The DMX port number of the request.
+ * @param[in] request_header The header of the RDM request.
+ * @param[in] response_header The header of the RDM response.
+ * @param[inout] context The user context provided to the function.
+ */
 typedef void (*rdm_callback_t)(dmx_port_t dmx_num, rdm_header_t *request_header,
                                rdm_header_t *response_header, void *context);
 
@@ -22,12 +28,9 @@ typedef void (*rdm_callback_t)(dmx_port_t dmx_num, rdm_header_t *request_header,
 }
 #endif
 
-// #include "rdm/responder/include/device_config.h"  // TODO
 #include "rdm/responder/include/device_control.h"
 #include "rdm/responder/include/discovery.h"
-// #include "rdm/responder/include/display_setting.h"  // TODO
 #include "rdm/responder/include/dmx_setup.h"
-// #include "rdm/responder/include/power_setting.h"  // TODO
 #include "rdm/responder/include/product_info.h"
 #include "rdm/responder/include/queue_status.h"
 #include "rdm/responder/include/rdm_info.h"
