f577fb853e05aca8fc5b3cee842c33b5c2558860
Mitch Weisbrod <mitch@theweisbrods.com>
2024-01-08 18:20:11 -0800

    documentation

--- src/rdm/responder/include/sensor_parameter.h
+++ src/rdm/responder/include/sensor_parameter.h
@@ -62,22 +62,66 @@ bool rdm_register_sensor_value(dmx_port_t dmx_num, uint8_t sensor_count,
 bool rdm_register_record_sensors(dmx_port_t dmx_num, rdm_callback_t cb,
                                  void *context);
 
-// TODO: docs
+/**
+ * @brief Returns the number of sensors supported by the device. This is the
+ * same number passed in the sensor_count argument of the
+ * rdm_register_sensor_value() function.
+ *
+ * @param dmx_num The DMX port number.
+ * @param sub_device The sub-device number.
+ * @return The number of sensors supported by the sub-device.
+ */
 uint8_t rdm_sensor_get_count(dmx_port_t dmx_num, rdm_sub_device_t sub_device);
 
-// TODO: docs
+/**
+ * @brief Copies the sensor values of a specified sensor into a user buffer.
+ *
+ * @param dmx_num The DMX port number.
+ * @param sub_device The sub-device number.
+ * @param sensor_num The sensor number.
+ * @param sensor_value A pointer to a rdm_sensor_value_t into which to store the
+ * sensor values.
+ * @return The number of bytes written to sensor_value.
+ */
 size_t rdm_sensor_get(dmx_port_t dmx_num, rdm_sub_device_t sub_device,
                       uint8_t sensor_num, rdm_sensor_value_t *sensor_value);
 
-// TODO: docs
+/**
+ * @brief Sets a value to a specified sensor. This function should be called
+ * periodically as RDM controllers may request sensor data at any given time.
+ *
+ * @param dmx_num The DMX port number.
+ * @param sub_device The sub-device number.
+ * @param sensor_num The sensor number.
+ * @param value The value to be written to the sensor.
+ * @return true on success.
+ * @return false on failure.
+ */
 bool rdm_sensor_set(dmx_port_t dmx_num, rdm_sub_device_t sub_device,
                     uint8_t sensor_num, int16_t value);
 
-// TODO: docs
+/**
+ * @brief Records the current value set in a specified sensor. The value
+ * recorded on the sensor is the last value that was set using rdm_sensor_set().
+ *
+ * @param dmx_num The DMX port number.
+ * @param sub_device The sub-device number.
+ * @param sensor_num The sensor number.
+ * @return true on success.
+ * @return false on failure.
+ */
 bool rdm_sensor_record(dmx_port_t dmx_num, rdm_sub_device_t sub_device,
                        uint8_t sensor_num);
 
-// TODO: docs
+/**
+ * @brief Resets all the values in a sensor to 0.
+ *
+ * @param dmx_num The DMX port number.
+ * @param sub_device The sub-device number.
+ * @param sensor_num The sensor number.
+ * @return true on success.
+ * @return false on failure.
+ */
 bool rdm_sensor_reset(dmx_port_t dmx_num, rdm_sub_device_t sub_device,
                       uint8_t sensor_num);
 
