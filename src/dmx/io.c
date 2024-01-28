3feab14de1096780c15b4778bb1cd99d7ddcfbfb
Mitch Weisbrod <mitch@theweisbrods.com>
2024-01-28 15:34:39 -0800

    allow for custom discovery packet spacing

--- src/dmx/io.c
+++ src/dmx/io.c
@@ -343,10 +343,8 @@ size_t dmx_send_num(dmx_port_t dmx_num, size_t size) {
         /* This is a condition in which the RDM controller sends an
           RDM_PID_DISC_UNIQUE_BRANCH message and a valid response has already
           been received. The RDM standard doesn't specify how long the RDM
-          controller should wait before sending the next RDM request. This
-          library makes an inference to what this duration should be in order to
-          reduce RDM discovery times. This inference is based on related
-          information from the RDM specification.*/
+          controller should wait before sending the next RDM request. Therefore
+          this value is customizable by the user in the Kconfig.*/
         timer_alarm = RDM_TIMING_CONTROLLER_DISCOVERY_TRANSACTION_MIN;
       } else {
         timer_alarm = RDM_TIMING_CONTROLLER_DISCOVERY_TO_REQUEST_MIN;
