cf6e533390c536dd147d16d4954bb7768403bf16
Mitch Weisbrod <mitch@theweisbrods.com>
2024-02-23 22:04:21 -0800

    add power/lamp settings

---
+++ src/rdm/responder/include/power_lamp.h
@@ -0,0 +1,22 @@
+/**
+ * @file rdm/responder/include/power_lamp.h
+ * @author Mitch Weisbrod
+ * @brief This file contains power and lamp setting functions for the RDM
+ * responder. The PIDs in power and lamp settings include RDM_PID_DEVICE_HOURS,
+ * RDM_PID_LAMP_HOURS, RDM_PID_LAMP_STRIKES, RDM_PID_LAMP_STATE,
+ * RDM_PID_LAMP_ON_MODE, and RDM_PID_DEVICE_POWER_CYCLES. This file also
+ * includes getters and setters for these functions as appropriate.
+ */
+#pragma once
+
+#include "dmx/include/types.h"
+#include "rdm/include/types.h"
+#include "rdm/responder.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
